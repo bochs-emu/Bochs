5f1e60753cf9e6471a62c02b228c3ff69c13cb37
Bryce Denney <bryce@tlw.com>
2001-04-10 02:51:33 +0000

    - call bx_snprintf instead of snprintf, and the configure and osdep.h   will take care of it.

--- bochs/debug/dbg_main.cc
+++ bochs/debug/dbg_main.cc
@@ -1262,7 +1262,7 @@ bx_dbg_symbolic_address(Bit32u context, Bit32u eip, Bit32u base)
 {
       static char buf[80];
       if (base != 0) {
-	    SNPRINTF0(buf, 80, "non-zero base");
+	    bx_snprintf (buf, 80, "non-zero base");
 	    return buf;
       }
       // Look up this context
@@ -1271,17 +1271,17 @@ bx_dbg_symbolic_address(Bit32u context, Bit32u eip, Bit32u base)
 	    // Try global context
 	    cntx = context_t::get_context(0);
 	    if (!cntx) {
-		  SNPRINTF0(buf, 80, "unknown context");
+		  bx_snprintf (buf, 80, "unknown context");
 		  return buf;
 	    }
       }
 
       symbol_entry_t* entr = cntx->get_symbol_entry(eip);
       if (!entr) {
-	    SNPRINTF0(buf, 80, "no symbol");
+	    bx_snprintf (buf, 80, "no symbol");
 	    return buf;
       }
-      SNPRINTF2(buf, 80, "%s+%x", entr->name, eip - entr->start);
+      bx_snprintf (buf, 80, "%s+%x", entr->name, eip - entr->start);
       return buf;
 }
 
