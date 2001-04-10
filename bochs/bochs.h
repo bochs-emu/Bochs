c90f74536d7c5ec4a06bd8e1c01104fc62a8f4ec
Bryce Denney <bryce@tlw.com>
2001-04-10 01:58:07 +0000

    - added informative header

--- bochs/bochs.h
+++ bochs/bochs.h
@@ -20,6 +20,12 @@
 //  License along with this library; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 
+//
+// bochs.h is the master header file for all C++ code.  It includes all 
+// the system header files needed by bochs, and also includes all the bochs
+// C++ header files.  Because bochs.h and the files that it includes has 
+// structure and class definitions, it cannot be called from C code.
+// 
 
 #ifndef BX_BOCHS_H
 #  define BX_BOCHS_H 1
