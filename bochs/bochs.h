100114287e17197b7f6c967ee0b33b59a7874ddb
Bryce Denney <bryce@tlw.com>
2001-04-10 11:24:56 +0000

    - add comments

--- bochs/bochs.h
+++ bochs/bochs.h
@@ -20,6 +20,7 @@
 //  License along with this library; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 
+// bochs.h can only be included by C++ files
 
 #ifndef BX_BOCHS_H
 #  define BX_BOCHS_H 1
