a6fef546783bcdd3c0fcce8cd4d6ac59cdccf80a
Bryce Denney <bryce@tlw.com>
2001-04-10 02:20:02 +0000

    - update copyright dates to 2001 for all mandrake headers - for bochs files with other header, replaced with current mandrake header

--- bochs/font/vga.bitmap.h
+++ bochs/font/vga.bitmap.h
@@ -1,3 +1,25 @@
+//  Copyright (C) 2001  MandrakeSoft S.A.
+//
+//    MandrakeSoft S.A.
+//    43, rue d'Aboukir
+//    75002 Paris - France
+//    http://www.linux-mandrake.com/
+//    http://www.mandrakesoft.com/
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+
 typedef struct {
   unsigned char data[16];
   } bx_fontcharbitmap_t;
