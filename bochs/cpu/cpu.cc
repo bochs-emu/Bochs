f36830e2933fdcc02c10d4945651b4c1062ef748
Bryce Denney <bryce@tlw.com>
2001-04-10 11:48:11 +0000

    - added conditionals around the local APIC call

--- bochs/cpu/cpu.cc
+++ bochs/cpu/cpu.cc
@@ -441,10 +441,15 @@ handle_async_event:
     Bit8u vector;
 
     // NOTE: similar code in ::take_irq()
+#if BX_APIC_SUPPORT
     if (BX_CPU_THIS_PTR int_from_local_apic)
       vector = local_apic.acknowledge_int ();
     else
       vector = BX_IAC(); // may set INTR with next interrupt
+#else
+    // if no local APIC, always acknowledge the PIC.
+    vector = BX_IAC(); // may set INTR with next interrupt
+#endif
     //if (bx_dbg.interrupts) bx_printf("decode: interrupt %u\n",
     //                                   (unsigned) vector);
     BX_CPU_THIS_PTR errorno = 0;
