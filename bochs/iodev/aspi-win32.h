9577b4277c784387da38e9a2280d9eb730c33657
Bryce Denney <bryce@tlw.com>
2001-06-25 12:52:37 +0000

    - only define PACKED if it's not already defined, since in Cygwin it's   already defined.

--- bochs/iodev/aspi-win32.h
+++ bochs/iodev/aspi-win32.h
@@ -1,9 +1,22 @@
+//
+// iodev/aspi-win32.h
+// $Id: aspi-win32.h,v 1.2 2001-06-25 12:52:37 bdenney Exp $
+//
+// This file was copied from cdrecord 1.9 under libscg/scg/aspi-win32.h.
+// The only modification is related to use of the PACKED keyword.  
+// 
+
 #ifndef __ASPI_WIN32_H_
 #define __ASPI_WIN32_H_
 
 #include <windows.h>
 
-#define PACKED
+#ifndef PACKED
+// It seems that VC++ has no PACKED keyword but Cygwin does.  We can just
+// define PACKED to be empty if it's not already defined by the system 
+// headers.
+#define PACKED /* empty */
+#endif
 
 /***************************************************************************
  ** SCSI MISCELLANEOUS EQUATES
