a10f33a8c79397535a8c9937f6cfcb8b8813f408
instinc <instinc>
2001-09-14 22:37:32 +0000

    Removed the structure from the class definition and used it directly in the object

--- bochs/iodev/iodebug.h
+++ bochs/iodev/iodebug.h
@@ -13,21 +13,17 @@ public:
   bx_iodebug_c( void );
   ~bx_iodebug_c( void );
   int init( bx_devices_c * );
-  void mem_write( BX_CPU_C *cpu, Bit32u addr, unsigned len, void *data);
-  void mem_read( BX_CPU_C *cpu, Bit32u addr, unsigned len, void *data);
+  static void mem_write( BX_CPU_C *cpu, Bit32u addr, unsigned len, void *data);
+  static void mem_read( BX_CPU_C *cpu, Bit32u addr, unsigned len, void *data);
+
 private:
   static Bit32u read_handler(void *this_ptr, Bit32u address, unsigned io_len);
   static void write_handler(void *this_ptr, Bit32u address, Bit32u value, unsigned io_len);
   Bit32u read(Bit32u addr, unsigned int io_len);
   void write(Bit32u addr, Bit32u dvalue, unsigned int io_len);
-  unsigned int range_test(Bit32u addr, unsigned int len);
+  static unsigned int range_test(Bit32u addr, unsigned int len);
+  static void add_range( Bit32u addr_start, Bit32u addr_end);
 
-  struct s_type {
-    Boolean enabled;
-    unsigned int register_select;
-    Bit32u monitored_mem_areas_start[BX_IODEBUG_MAX_AREAS];
-    Bit32u monitored_mem_areas_end[BX_IODEBUG_MAX_AREAS];
-  } s;
 
   bx_devices_c *devices;
 };
