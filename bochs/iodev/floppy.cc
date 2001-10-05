b974699ce612ffe2d7209f74b28541df873b0535
Bryce Denney <bryce@tlw.com>
2001-10-05 14:01:52 +0000

    - include <windows.h> on win32 platforms so that we get a definition of   wsprintf

--- bochs/iodev/floppy.cc
+++ bochs/iodev/floppy.cc
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: floppy.cc,v 1.24 2001-10-03 13:10:38 bdenney Exp $
+// $Id: floppy.cc,v 1.25 2001-10-05 14:01:52 bdenney Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2001  MandrakeSoft S.A.
@@ -40,6 +40,9 @@ extern "C" {
 #include <errno.h>
 }
 #include "bochs.h"
+#ifdef WIN32
+#include <windows.h>     // for wsprintf
+#endif
 #define LOG_THIS bx_floppy.
 
 
@@ -84,7 +87,7 @@ bx_floppy_ctrl_c::~bx_floppy_ctrl_c(void)
   void
 bx_floppy_ctrl_c::init(bx_devices_c *d, bx_cmos_c *cmos)
 {
-	BX_DEBUG(("Init $Id: floppy.cc,v 1.24 2001-10-03 13:10:38 bdenney Exp $"));
+	BX_DEBUG(("Init $Id: floppy.cc,v 1.25 2001-10-05 14:01:52 bdenney Exp $"));
   BX_FD_THIS devices = d;
 
   BX_FD_THIS devices->register_irq(6, "Floppy Drive");
