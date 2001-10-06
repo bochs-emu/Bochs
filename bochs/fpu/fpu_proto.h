de40494484f877dccf8cea6a228b7af30cf6b4a4
Bryce Denney <bryce@tlw.com>
2001-10-06 04:35:13 +0000

    - oops, the script that added the headers in fpu went wild here.  fixed.

--- bochs/fpu/fpu_proto.h
+++ bochs/fpu/fpu_proto.h
@@ -1,6 +1,38 @@
+/////////////////////////////////////////////////////////////////////////
+// $Id: fpu_proto.h,v 1.4 2001-10-06 04:35:13 bdenney Exp $
+/////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2001  MandrakeSoft S.A.
+//
+//    MandrakeSoft S.A.
+//    43, rue d'Aboukir
+//    75002 Paris - France
+//    http://www.linux-mandrake.com/
+//    http://www.mandrakesoft.com/
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+
+//
+// bochs.h is the master header file for all C++ code.  It includes all 
+// the system header files needed by bochs, and also includes all the bochs
+// C++ header files.  Because bochs.h and the files that it includes has 
+// structure and class definitions, it cannot be called from C code.
+// 
+
 #ifndef _FPU_PROTO_H
 #define _FPU_PROTO_H
-|  $Id: fpu_proto.h,v 1.3 2001-10-06 03:53:46 bdenney Exp $
 
 /* errors.c */
 extern void Un_impl(void);
