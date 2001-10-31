44c3419cb756040fc274b49d15ac1a444f949907
Bryce Denney <bryce@tlw.com>
2001-10-31 16:03:23 +0000

    - from Psyon, later on October 18, 2001:   Ok, This one gives a descriptin of the adapters now also.  Like this:
      Description
        \Device\Packet_BLAH
    
      and you would put \Device\Packet_BLAH in .bochsrc
    
      It only works with Visual C++ in debug mode for some reason... Ill get it
      figured out I guess.

--- bochs/misc/niclist.c
+++ bochs/misc/niclist.c
@@ -2,7 +2,7 @@
 //
 // misc/niclist.c
 // by Don Becker <x-odus@iname.com>
-// $Id: niclist.c,v 1.2 2001-10-31 16:01:29 bdenney Exp $
+// $Id: niclist.c,v 1.3 2001-10-31 16:03:23 bdenney Exp $
 //
 // This program is for win32 only.  It lists the network interface cards
 // that you can use in the "ethdev" field of the ne2k line in your bochsrc.
@@ -15,88 +15,157 @@
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <wchar.h>
 
-BOOLEAN   (*PacketGetAdapterNames) (PTSTR, PULONG);
+typedef struct _Adapter {
+	char description[256];
+	char device[512];
+} ADAPTER;
+
+BOOLEAN   (*PacketGetAdapterNames)(PTSTR, PULONG) = NULL;
+
+int HexDump(long start, unsigned char *buf, int len)
+{
+   int i;
+
+   if( !buf )
+   {
+      return( 0 );
+   }
+
+   if( len == 0L )
+   {
+      return( 0 );
+   }
+
+   while( len > 0L )
+   {
+      printf( "%6.6X: ", start );
+      for( i = 0; i < 8; ++i )
+      {
+         if( len - i > 0 )
+         {
+            printf( " %2.2X", buf[ i ] & 0xFF );
+         }
+         else
+         {
+            printf( "   " );
+         }
+      }
+
+      printf( " :" );
+
+      for( i = 8; i < 16; ++i )
+      {
+         if( len - i > 0 )
+         {
+            printf( " %2.2X", buf[ i ] & 0xFF );
+         }
+         else
+         {
+            printf( "   " );
+         }
+      }
+
+      printf( "    " );
+
+      for( i = 0; i < 16; ++i )
+      {
+         if( len - i > 0 )
+         {
+            if( isprint( buf[ i ] ) )
+            {
+               printf( "%c", buf[ i ] );
+            }
+            else
+            {
+               printf( "%c", '.' );
+            }
+         }
+         else
+         {
+            printf( "%c", '.' );
+         }
+      }
+
+
+      printf( "\n" );
+
+      len -= 16;
+      start += 16L;
+      buf += 16;
+   }
+
+   return( 0 );
+}
 
 int main(int argc, char **argv)
 {
-    int i = 0;
-    wchar_t   AdapterNameW[512];
-    char      AdapterNameA[512];  
-    int       AdapterNum = 0;
-    ULONG     AdapterLength;
-    HINSTANCE hPacket;
-    DWORD     dwVersion, dwMajorVersion;
-    char      AdapterList[10][1024];
-    wchar_t   *tempW, *tempW2;
-    char      *tempA, *tempA2;
-    char      buff[512];
+	int i, n;
+	char          AdapterInfo[1024];
+	char          *device, *desc, *brk;
+	unsigned long AdapterLength;
+	ADAPTER       Adapters[20];
+	HINSTANCE     hPacket;
+	DWORD         dwVersion, dwMajorVersion;
+	char          *split, *end;
+	int           splitlen = 0;
+	int           endlen = 0;
 
-    hPacket = LoadLibrary("PACKET.DLL");
-
-	if(hPacket) {
-          PacketGetAdapterNames = (BOOLEAN   (*)(PTSTR, PULONG))                   GetProcAddress(hPacket, "PacketGetAdapterNames");
-      } else {
-          printf("Could not load WinPCap driver!\n");
-		  return 1;
-     }
-
+	hPacket = LoadLibrary("PACKET.DLL");
+    if(hPacket) {
+		PacketGetAdapterNames = (BOOLEAN (*)(PTSTR, PULONG))GetProcAddress(hPacket, "PacketGetAdapterNames");
+	} else {
+		printf("Could not load WinPCap driver!\n");
+		return 1;
+	}
+	
 	dwVersion      = GetVersion();
 	dwMajorVersion = (DWORD)(LOBYTE(LOWORD(dwVersion)));
-	if (!(dwVersion >= 0x80000000 && dwMajorVersion >= 4))
-	{  // Windows NT
-		PacketGetAdapterNames((char *)AdapterNameW,&AdapterLength);
-		tempW  = AdapterNameW;
-		tempW2 = AdapterNameW;
-		while ((*tempW!='\0')||(*(tempW-1)!='\0'))
-		{
-			if (*tempW=='\0') 
-			{
-				memcpy(AdapterList[i],tempW2,(tempW-tempW2)*2);
-				tempW2=tempW+1;
-				i++;
-		}
-	
-		tempW++;
-		}
-	  
-		AdapterNum=i;
-		for (i=0;i<AdapterNum;i++) {
-#if defined(__CYGWIN__) || defined(__MINGW32__)
-                        memset(&buff, 0, sizeof(buff));
-                        wcstombs(buff, (wchar_t *)AdapterList[i], wcslen((wchar_t *)AdapterList[i]));
-			printf("\n%d.) %s\n",i+1,buff);
-#else
-                        wprintf(L"\n%d.) %s\n",i+1,AdapterList[i]);
-#endif
-                }
-		printf("\n");
-		
+	if(!(dwVersion >= 0x80000000 && dwMajorVersion >= 4)) {
+		// Windows NT
+		split    = "\0\0";
+		splitlen = 2;
+		end      = "\0\0\0\0";
+		endlen   = 4;
+	} else {
+		// Windows 9x
+		split    = "\0";
+		splitlen = 1;
+		end      = "\0\0";
+		endlen   = 2;
 	}
 
-	else	//windows 95
-	{
-		PacketGetAdapterNames(AdapterNameA,&AdapterLength);
-		tempA  = AdapterNameA;
-		tempA2 = AdapterNameA;
+	PacketGetAdapterNames((char *)AdapterInfo, &AdapterLength);
+	device = AdapterInfo;
 
-		while ((*tempA!='\0')||(*(tempA-1)!='\0'))
-		{
-			if (*tempA=='\0') 
-			{
-				memcpy(AdapterList[i],tempA2,tempA-tempA2);
-				tempA2=tempA+1;
-				i++;
-			}
-			tempA++;
+	brk = device;
+	i = 0;
+	while(memcmp(brk, end, endlen) != 0) {
+		while(memcmp(brk, split, splitlen) != 0) {
+			strncat(Adapters[i].device, brk, 1);
+			brk += splitlen;
 		}
-		  
-		AdapterNum=i;
-		for (i=0;i<AdapterNum;i++)
-			printf("\n%d.) %s\n",i+1,AdapterList[i]);
-		printf("\n");
+		i++;
+		if(memcmp(brk, end, endlen) == 0) break;
+		brk += splitlen;
+	}
+	n = i;
+	brk += endlen;
+	desc = brk;
 
+	i = 0;
+	brk = desc;
+	while(*brk != '\0' && *(brk+1) != '\0') {
+		while(*brk != '\0') brk++;
+		brk++;
+		strcpy(Adapters[i].description, desc);
+		i++;
+		desc = brk;
+	}
+	n = i;
+
+	for(i = 0; i < n; i++) {
+		printf("%s\n  %s\n\n", Adapters[i].description, Adapters[i].device);
 	}
 	return 0;
 }
