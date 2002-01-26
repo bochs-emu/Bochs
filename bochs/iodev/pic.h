6461cb2ab9e1fc3307ac930569232c5a47497568
Volker Ruppert <Volker.Ruppert@t-online.de>
2002-01-26 12:03:10 +0000

    - new IRQ signal handling prepared

--- bochs/iodev/pic.h
+++ bochs/iodev/pic.h
@@ -1,8 +1,8 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: pic.h,v 1.4 2001-10-03 13:10:38 bdenney Exp $
+// $Id: pic.h,v 1.5 2002-01-26 12:03:10 vruppert Exp $
 /////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (C) 2001  MandrakeSoft S.A.
+//  Copyright (C) 2002  MandrakeSoft S.A.
 //
 //    MandrakeSoft S.A.
 //    43, rue d'Aboukir
@@ -53,6 +53,7 @@ typedef struct {
   Bit8u read_reg_select;   /* 0=IRR, 1=ISR */
   Bit8u irq;               /* current IRQ number */
   Boolean INT;             /* INT request pin of PIC */
+  Boolean IRQ_line[8];     /* IRQ pins of PIC */
   struct {
     Boolean    in_init;
     Boolean    requires_4;
@@ -68,6 +69,8 @@ public:
   bx_pic_c(void);
   ~bx_pic_c(void);
   BX_PIC_SMF void   init(bx_devices_c *);
+  BX_PIC_SMF void   lower_irq(unsigned irq_no);
+  BX_PIC_SMF void   raise_irq(unsigned irq_no);
   BX_PIC_SMF void   trigger_irq(unsigned irq_no);
   BX_PIC_SMF void   untrigger_irq(unsigned irq_no);
   BX_PIC_SMF Bit8u  IAC(void);
