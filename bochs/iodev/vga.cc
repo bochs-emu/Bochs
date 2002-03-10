0e58d8a083b27017b120a0886c207e819e7cf05d
Jeroen Janssen <japj@xs4all.nl>
2002-03-10 20:59:11 +0000

    - VBE adding missing set bpp (caused a panic with current vgabios)

--- bochs/iodev/vga.cc
+++ bochs/iodev/vga.cc
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: vga.cc,v 1.24 2002-03-10 04:51:24 bdenney Exp $
+// $Id: vga.cc,v 1.25 2002-03-10 20:59:11 japj Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2002  MandrakeSoft S.A.
@@ -2197,7 +2197,12 @@ bx_vga_c::vbe_write(Bit32u address, Bit32u value, unsigned io_len)
       {
         BX_VGA_THIS s.vbe_yres=(Bit16u) value;
       } break;
-      
+  
+      case VBE_DISPI_INDEX_BPP: // set bpp
+      {
+        BX_VGA_THIS s.vbe_bpp=(Bit16u) value;
+      } break;
+        
       case VBE_DISPI_INDEX_BANK: // set bank
       {
         BX_INFO(("VBE set bank to %d", BX_VGA_THIS s.vbe_bank));
