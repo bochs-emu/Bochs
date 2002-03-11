70e007b8f7468d0213b92dde5d3d13ef5b585456
Bryce Denney <bryce@tlw.com>
2002-03-11 15:13:38 +0000

    - add comment

--- bochs/gui/keymaps/convertmap.pl
+++ bochs/gui/keymaps/convertmap.pl
@@ -1,4 +1,8 @@
 #!/usr/bin/perl
+# little utility script that I used to convert key map files from
+# the pre-March 11 format to the post-March 11 format.  It doesn't
+# do anything smart with the ascii equivalents and modifiers, so ATM those must
+# be added by hand.
 
 while (<STDIN>)
 {
