6881dbd848bcb50aa77202970a7618945490157c
Bryce Denney <bryce@tlw.com>
2002-03-27 03:47:45 +0000

    - only print the first 10 copies of "WARNING: Local APIC Processor Priority not   implemented" to avoid slowing sim down to a crawl.

--- bochs/cpu/apic.cc
+++ bochs/cpu/apic.cc
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: apic.cc,v 1.12 2002-03-25 01:58:34 bdenney Exp $
+// $Id: apic.cc,v 1.13 2002-03-27 03:47:45 bdenney Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 #define NEED_CPU_REG_SHORTCUTS 1
@@ -683,7 +683,11 @@ bx_local_apic_c::get_delivery_bitmask (Bit8u dest, Bit8u dest_mode)
 
 Bit8u bx_local_apic_c::get_ppr ()
 {
-  BX_ERROR(("WARNING: Local APIC Processor Priority not implemented, returning 0"));
+  static int warned = 0;
+  if (warned < 10) {
+    BX_ERROR(("WARNING: Local APIC Processor Priority not implemented, returning 0"));
+    warned++;
+  }
   // should look at TPR, vector of highest priority isr, etc.
   return 0;
 }
