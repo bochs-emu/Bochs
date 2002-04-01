4e17b972110215fe0eda041395e29e1bcf084968
instinc <instinc>
2002-04-01 04:12:11 +0000

    fixed feature request 488645

--- bochs/gui/control.cc
+++ bochs/gui/control.cc
@@ -1,10 +1,10 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: control.cc,v 1.44 2002-03-26 14:46:02 bdenney Exp $
+// $Id: control.cc,v 1.45 2002-04-01 04:12:11 instinc Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 /*
  * gui/control.cc
- * $Id: control.cc,v 1.44 2002-03-26 14:46:02 bdenney Exp $
+ * $Id: control.cc,v 1.45 2002-04-01 04:12:11 instinc Exp $
  *
  * This is code for a text-mode control panel.  Note that this file
  * does NOT include bochs.h.  Instead, it does all of its contact with
@@ -645,6 +645,8 @@ ask:
 	return SIM->notify_return(-1);
       // return 0 for continue, 1 for alwayscontinue, 2 for die, 3 for debug.
       if (!BX_HAVE_ABORT && choice==3) goto ask;
+      fflush(stdout);
+      fflush(stderr);
       SIM->notify_return(choice);
     }
     break;
