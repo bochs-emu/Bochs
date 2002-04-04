3d49380b3577dc503b6d6adc195d855ff042b63d
Volker Ruppert <Volker.Ruppert@t-online.de>
2002-04-04 18:11:53 +0000

    - send a scancode for "left control key released" before handling the scancode   for the "AltGr" key (present on European keyboards)

--- bochs/gui/win32.cc
+++ bochs/gui/win32.cc
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: win32.cc,v 1.30 2002-03-28 01:12:26 bdenney Exp $
+// $Id: win32.cc,v 1.31 2002-04-04 18:11:53 vruppert Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2002  MandrakeSoft S.A.
@@ -626,6 +626,11 @@ void bx_gui_c::handle_events(void) {
     }
     else {
       if (((key & 0x0100) && ((key & 0x01ff) != 0x0145)) | ((key & 0x01ff) == 0x45)) {
+        // This makes the "AltGr" key on European keyboards work
+	if (key==0x138) {
+          scancode = 0x9d; // left control key released
+          bx_devices.keyboard->put_scancode(&scancode, 1);
+	}
         // Its an extended key
         scancode = 0xE0;
         bx_devices.keyboard->put_scancode(&scancode, 1);
