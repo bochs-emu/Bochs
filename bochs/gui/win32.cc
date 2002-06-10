b539c1d369c85feb853cdc9b35919f38de531ad9
Christophe Bothamy <cbbochs@free.fr>
2002-06-10 21:12:26 +0000

    - backported changes r1.31 and 1.32 to REL_1_4 branch

--- bochs/gui/win32.cc
+++ bochs/gui/win32.cc
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: win32.cc,v 1.30 2002-03-28 01:12:26 bdenney Exp $
+// $Id: win32.cc,v 1.30.2.1 2002-06-10 21:12:26 cbothamy Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2002  MandrakeSoft S.A.
@@ -625,7 +625,14 @@ void bx_gui_c::handle_events(void) {
       headerbar_click(LOWORD(key));
     }
     else {
-      if (((key & 0x0100) && ((key & 0x01ff) != 0x0145)) | ((key & 0x01ff) == 0x45)) {
+      // Swap the scancodes of "numlock" and "pause"
+      if ((key & 0xff)==0x45) key ^= 0x100;
+      if (key & 0x0100) {
+        // This makes the "AltGr" key on European keyboards work
+	if (key==0x138) {
+          scancode = 0x9d; // left control key released
+          bx_devices.keyboard->put_scancode(&scancode, 1);
+	}
         // Its an extended key
         scancode = 0xE0;
         bx_devices.keyboard->put_scancode(&scancode, 1);
