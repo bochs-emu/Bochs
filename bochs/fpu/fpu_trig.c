f249d2f78dda489e4205101aeb9537c58bab4bcc
Volker Ruppert <Volker.Ruppert@t-online.de>
2002-07-03 20:13:31 +0000

    - bugfix in rem_kernel(): the second and third stage of the calculation   modifies only the msw of variable "x".

--- bochs/fpu/fpu_trig.c
+++ bochs/fpu/fpu_trig.c
@@ -1,6 +1,6 @@
 /*---------------------------------------------------------------------------+
  |  fpu_trig.c                                                               |
- |  $Id: fpu_trig.c,v 1.4 2001-10-06 03:53:46 bdenney Exp $
+ |  $Id: fpu_trig.c,v 1.5 2002-07-03 20:13:31 vruppert Exp $
  |                                                                           |
  | Implementation of the FPU "transcendental" functions.                     |
  |                                                                           |
@@ -869,11 +869,11 @@ static void rem_kernel(u64 st0, u64 *y, u64 st1, u64 q, int n)
 
   work = st1 >> 32;
   work *= (u32)q;
-  x -= work;
+  x -= work << 32;
 
   work = (u32)st1;
   work *= q >> 32;
-  x -= work;
+  x -= work << 32;
   
 #else
   int dummy;
