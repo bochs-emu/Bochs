edc4d5045f0262710bc36442120e5f8a29c964db
Bryce Denney <bryce@tlw.com>
2002-08-26 18:05:16 +0000

    - removed -nocontrolpanel and -nocp arguments.  It was never documented,   and is now replaced by -q, which IS documented thanks to Volker.

--- bochs/main.cc
+++ bochs/main.cc
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: main.cc,v 1.120 2002-08-26 15:31:19 bdenney Exp $
+// $Id: main.cc,v 1.121 2002-08-26 18:05:16 bdenney Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2002  MandrakeSoft S.A.
@@ -1186,12 +1186,9 @@ bx_do_text_config_interface (int argc, char *argv[])
   char *bochsrc = NULL;
   int norcfile = 1;
 
-  // detect -q, -qf, -nocontrolpanel or -nocp argument before anything else
+  // detect -q, -qf argument before anything else
   int arg = 1;
-  if ((argc > 1) && 
-       ((!strcmp ("-nocontrolpanel", argv[1]))
-        || (!strcmp ("-nocp", argv[1]))
-        || (!strncmp ("-q", argv[1], 2)))) {
+  if ((argc > 1) && !strncmp ("-q", argv[1], 2)) {
     // skip the configuration interface
     arg++;
     enable_config_interface = 0;
