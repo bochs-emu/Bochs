f24624f27a24194cc5779ee441b5a78ad41a5d4e
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2002-09-28 17:09:04 +0000

    Replaced instrumentation example0 files with new example, for new implementation of the instrumentation

--- bochs/instrument/example0/instrument.h
+++ bochs/instrument/example0/instrument.h
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: instrument.h,v 1.4 2001-10-03 13:10:38 bdenney Exp $
+// $Id: instrument.h,v 1.5 2002-09-28 17:09:04 sshwarts Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2001  MandrakeSoft S.A.
@@ -25,18 +25,19 @@
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 
 
-
 // possible types passed to BX_INSTR_TLB_CNTRL()
-#define BX_INSTR_MOV_CR3     10
-#define BX_INSTR_INVLPG      11
-#define BX_INSTR_TASKSWITCH  12
+#define BX_INSTR_MOV_CR3      10
+#define BX_INSTR_INVLPG       11
+#define BX_INSTR_TASKSWITCH   12
 
 // possible types passed to BX_INSTR_CACHE_CNTRL()
-#define BX_INSTR_INVD      20
-#define BX_INSTR_WBINVD    21
-
-
-#if BX_INSTRUMENTATION
+#define BX_INSTR_INVD         20
+#define BX_INSTR_WBINVD       21
+#define BX_INSTR_SFENCE       22
+#define BX_INSTR_PREFETCH_T0  23
+#define BX_INSTR_PREFETCH_T1  24
+#define BX_INSTR_PREFETCH_T2  25
+#define BX_INSTR_PREFETCH_NTA 26
 
 #define BX_INSTR_IS_CALL  10
 #define BX_INSTR_IS_RET   11
@@ -44,117 +45,173 @@
 #define BX_INSTR_IS_JMP   13
 #define BX_INSTR_IS_INT   14
 
-void bx_instr_init ();
-void bx_instr_shutdown ();
-void bx_instr_opcode_begin(Bit32u linear);
-void bx_instr_opcode_end(Bit32u linear);
-void bx_instr_fetch_byte(Bit8u val8);
-void bx_instr_fetch_word(Bit16u val16);
-void bx_instr_fetch_dword(Bit32u val32);
-void bx_instr_interrupt(unsigned vector);
-void bx_instr_exception(unsigned vector);
-void bx_instr_inp(Bit16u addr, unsigned len);
-void bx_instr_outp(Bit16u addr, unsigned len);
-void bx_instr_cnear_branch_taken(Bit32u new_eip);
-void bx_instr_cnear_branch_not_taken(void);
-void bx_instr_ucnear_branch(unsigned what, Bit32u new_eip);
-void bx_instr_far_branch(unsigned what, Bit32u new_cs, Bit32u new_eip);
-void bx_instr_lin_read(Bit32u lin, Bit32u phy, unsigned len);
-void bx_instr_lin_write(Bit32u lin, Bit32u phy, unsigned len);
-
-#define BX_INSTR_INIT()                      bx_instr_init ();
-#define BX_INSTR_SHUTDOWN()                  bx_instr_shutdown ();
-#define BX_INSTR_OPCODE_BEGIN(linear)        bx_instr_opcode_begin(linear)
-#define BX_INSTR_OPCODE_END(linear)          bx_instr_opcode_end(linear)
-#define BX_INSTR_FETCH_BYTE(val8)            bx_instr_fetch_byte(val8)
-#define BX_INSTR_FETCH_WORD(val16)           bx_instr_fetch_word(val16)
-#define BX_INSTR_FETCH_DWORD(val32)          bx_instr_fetch_dword(val32)
-#define BX_INSTR_INTERRUPT(vector)           bx_instr_interrupt(vector)
-#define BX_INSTR_EXCEPTION(vector)           bx_instr_exception(vector)
-#define BX_INSTR_INP(addr, len)              bx_instr_inp(addr, len)
-#define BX_INSTR_OUTP(addr, len)             bx_instr_outp(addr, len)
-#define BX_INSTR_CNEAR_BRANCH_TAKEN(new_eip) bx_instr_cnear_branch_taken(new_eip)
-#define BX_INSTR_CNEAR_BRANCH_NOT_TAKEN()    bx_instr_cnear_branch_not_taken()
-#define BX_INSTR_UCNEAR_BRANCH(what, new_eip) bx_instr_ucnear_branch(what, new_eip)
-#define BX_INSTR_FAR_BRANCH(what, new_cs, new_eip) bx_instr_far_branch(what, new_cs, new_eip)
-#define BX_INSTR_LIN_READ(lin, phy, len)  bx_instr_lin_read(lin, phy, len)
-#define BX_INSTR_LIN_WRITE(lin, phy, len) bx_instr_lin_write(lin, phy, len)
 
 
-// called from the debug prompt
-void bx_instr_start(void);
-void bx_instr_stop(void);
-void bx_instr_reset(void);
-void bx_instr_print(void);
+#if BX_INSTRUMENTATION
 
-#else
+// called from the CPU core
 
-#define BX_INSTR_OPCODE_BEGIN(linear)
-#define BX_INSTR_OPCODE_END(linear)
-#define BX_INSTR_FETCH_BYTE(val8)
-#define BX_INSTR_FETCH_WORD(val16)
-#define BX_INSTR_FETCH_DWORD(val32)
-#define BX_INSTR_INTERRUPT(vector)
-#define BX_INSTR_EXCEPTION(vector)
-#define BX_INSTR_INP(addr, len)
-#define BX_INSTR_OUTP(addr, len)
-#define BX_INSTR_CNEAR_BRANCH_TAKEN(new_eip)
-#define BX_INSTR_CNEAR_BRANCH_NOT_TAKEN()
-#define BX_INSTR_UCNEAR_BRANCH(what, new_eip)
-#define BX_INSTR_FAR_BRANCH(what, new_cs, new_eip)
-#define BX_INSTR_LIN_READ(lin, phy, len)
-#define BX_INSTR_LIN_WRITE(lin, phy, len)
+void bx_instr_reset(unsigned cpu);
+void bx_instr_new_instruction(unsigned cpu);
 
-#endif
+void bx_instr_cnear_branch_taken(unsigned cpu, bx_address new_eip);
+void bx_instr_cnear_branch_not_taken(unsigned cpu);
+void bx_instr_ucnear_branch(unsigned cpu, unsigned what, bx_address new_eip);
+void bx_instr_far_branch(unsigned cpu, unsigned what, Bit16u new_cs, bx_address new_eip);
 
-// rest are no-ops
-#define BX_INSTR_OPCODE_BYTE1(b)
-#define BX_INSTR_OPCODE_BYTE2(b)
-#define BX_INSTR_OPCODE_G1EbIb(nnn)
-#define BX_INSTR_OPCODE_G1EvIv(nnn)
-#define BX_INSTR_OPCODE_G1EvIb(nnn)
-#define BX_INSTR_OPCODE_G2EbIb(nnn)
-#define BX_INSTR_OPCODE_G2EvIb(nnn)
-#define BX_INSTR_OPCODE_G2Eb1(nnn)
-#define BX_INSTR_OPCODE_G2Ev1(nnn)
-#define BX_INSTR_OPCODE_G2EbCL(nnn)
-#define BX_INSTR_OPCODE_G2EvCL(nnn)
-#define BX_INSTR_OPCODE_G3Eb(nnn)
-#define BX_INSTR_OPCODE_G3Ev(nnn)
-#define BX_INSTR_OPCODE_G4(nnn)
-#define BX_INSTR_OPCODE_G5(nnn)
-#define BX_INSTR_OPCODE_G6(nnn)
-#define BX_INSTR_OPCODE_G7(nnn)
-#define BX_INSTR_OPCODE_G8EvIb(nnn)
-#define BX_INSTR_MEM_CODE(linear, size)
-#define BX_INSTR_MEM_DATA(linear, size, rw)
-#define BX_INSTR_TLB_CNTRL(what, newval)
-#define BX_INSTR_CACHE_CNTRL(what)
-#define BX_INSTR_HWINTERRUPT(vector, cs, eip)
-#define BX_INSTR_OPCODE_REPEATING()
+void bx_instr_opcode(unsigned cpu, Bit8u *opcode, unsigned len, Boolean is32);
+void bx_instr_fetch_decode_completed(unsigned cpu, BxInstruction_t *i);
 
-#define BX_INSTR_PREFIX_AS()
-#define BX_INSTR_PREFIX_OS()
-#define BX_INSTR_PREFIX_REP()
-#define BX_INSTR_PREFIX_REPNE()
-#define BX_INSTR_PREFIX_LOCK()
-#define BX_INSTR_PREFIX_CS()
-#define BX_INSTR_PREFIX_SS()
-#define BX_INSTR_PREFIX_DS()
-#define BX_INSTR_PREFIX_ES()
-#define BX_INSTR_PREFIX_FS()
-#define BX_INSTR_PREFIX_GS()
+void bx_instr_prefix_as(unsigned cpu);
+void bx_instr_prefix_os(unsigned cpu);
+void bx_instr_prefix_rep(unsigned cpu);
+void bx_instr_prefix_repne(unsigned cpu);
+void bx_instr_prefix_lock(unsigned cpu);
+void bx_instr_prefix_cs(unsigned cpu);
+void bx_instr_prefix_ss(unsigned cpu);
+void bx_instr_prefix_ds(unsigned cpu);
+void bx_instr_prefix_es(unsigned cpu);
+void bx_instr_prefix_fs(unsigned cpu);
+void bx_instr_prefix_gs(unsigned cpu);
+void bx_instr_prefix_extend8b(unsigned cpu);
 
-#define BX_INSTR_MODRM32(modrm)
-#define BX_INSTR_SIB32(sib)
-#define BX_INSTR_MODRM16(modrm)
-#define BX_INSTR_SIB_mod0_base5(ss)
-#define BX_INSTR_SIB_MOD0_IND4()
-#define BX_INSTR_SIB_MOD1_IND4()
-#define BX_INSTR_SIB_MOD2_IND4()
+void bx_instr_interrupt(unsigned cpu, unsigned vector);
+void bx_instr_exception(unsigned cpu, unsigned vector);
+void bx_instr_hwinterrupt(unsigned cpu, unsigned vector, Bit16u cs, bx_address eip);
 
-#define BX_INSTR_IRET()
-#define BX_INSTR_DEBUG_PROMPT()
+void bx_instr_lin_read(unsigned cpu, bx_address lin, bx_address phy, unsigned len);
+void bx_instr_lin_write(unsigned cpu, bx_address lin, bx_address phy, unsigned len);
 
-#define BX_INSTR_PHY_WRITE(addr, len)
-#define BX_INSTR_PHY_READ(addr, len)
+/* simulation init, shutdown, reset */
+#  define BX_INSTR_INIT(cpu_id)
+#  define BX_INSTR_SHUTDOWN(cpu_id)
+#  define BX_INSTR_RESET(cpu_id)           bx_instr_reset(cpu_id)
+#  define BX_INSTR_NEW_INSTRUCTION(cpu_id) bx_instr_new_instruction(cpu_id)
+
+/* called from command line debugger */
+#  define BX_INSTR_DEBUG_PROMPT()        
+#  define BX_INSTR_START()               
+#  define BX_INSTR_STOP()                
+#  define BX_INSTR_PRINT()               
+
+/* branch resoultion */
+#  define BX_INSTR_CNEAR_BRANCH_TAKEN(cpu_id, new_eip)       bx_instr_cnear_branch_taken(cpu_id, new_eip)
+#  define BX_INSTR_CNEAR_BRANCH_NOT_TAKEN(cpu_id)   bx_instr_cnear_branch_not_taken(cpu_id)
+#  define BX_INSTR_UCNEAR_BRANCH(cpu_id, what, new_eip)      bx_instr_ucnear_branch(cpu_id, what, new_eip)
+#  define BX_INSTR_FAR_BRANCH(cpu_id, what, new_cs, new_eip) bx_instr_far_branch(cpu_id, what, new_cs, new_eip)
+
+/* decoding completed */
+#  define BX_INSTR_OPCODE(cpu_id, opcode, len, is32) \
+                       bx_instr_opcode(cpu_id, opcode, len, is32)
+#  define BX_INSTR_FETCH_DECODE_COMPLETED(cpu_id, i) \
+                       bx_instr_fetch_decode_completed(cpu_id, i)
+     
+/* prefix decoded */
+#  define BX_INSTR_PREFIX_AS(cpu_id)       bx_instr_prefix_as(cpu_id)
+#  define BX_INSTR_PREFIX_OS(cpu_id)       bx_instr_prefix_os(cpu_id)
+#  define BX_INSTR_PREFIX_REP(cpu_id)      bx_instr_prefix_rep(cpu_id)
+#  define BX_INSTR_PREFIX_REPNE(cpu_id)    bx_instr_prefix_repne(cpu_id)
+#  define BX_INSTR_PREFIX_LOCK(cpu_id)     bx_instr_prefix_lock(cpu_id)
+#  define BX_INSTR_PREFIX_CS(cpu_id)       bx_instr_prefix_cs(cpu_id)
+#  define BX_INSTR_PREFIX_SS(cpu_id)       bx_instr_prefix_ss(cpu_id)
+#  define BX_INSTR_PREFIX_DS(cpu_id)       bx_instr_prefix_ds(cpu_id)
+#  define BX_INSTR_PREFIX_ES(cpu_id)       bx_instr_prefix_es(cpu_id)
+#  define BX_INSTR_PREFIX_FS(cpu_id)       bx_instr_prefix_fs(cpu_id)
+#  define BX_INSTR_PREFIX_GS(cpu_id)       bx_instr_prefix_gs(cpu_id)
+#  define BX_INSTR_PREFIX_EXTEND8B(cpu_id) bx_instr_prefix_extend8b(cpu_id)
+
+/* exceptional case and interrupt */
+#  define BX_INSTR_EXCEPTION(cpu_id, vector)            bx_instr_exception(cpu_id, vector)
+#  define BX_INSTR_INTERRUPT(cpu_id, vector)            bx_instr_interrupt(cpu_id, vector)
+#  define BX_INSTR_HWINTERRUPT(cpu_id, vector, cs, eip) bx_instr_hwinterrupt(cpu_id, vector, cs, eip)
+
+/* TLB/CACHE control instruction executed */
+#  define BX_INSTR_CACHE_CNTRL(cpu_id, what)
+#  define BX_INSTR_TLB_CNTRL(cpu_id, what, newval)
+
+#  define BX_INSTR_REPEAT_ITERATION(cpu_id)
+
+/* memory access */
+#  define BX_INSTR_LIN_READ(cpu_id, lin, phy, len)      bx_instr_lin_read(cpu_id, lin, phy, len)
+#  define BX_INSTR_LIN_WRITE(cpu_id, lin, phy, len)     bx_instr_lin_write(cpu_id, lin, phy, len)
+
+#  define BX_INSTR_MEM_CODE(cpu_id, linear, size)
+#  define BX_INSTR_MEM_DATA(cpu_id, linear, size, rw)
+
+/* called from memory object */
+#  define BX_INSTR_PHY_WRITE(addr, len)
+#  define BX_INSTR_PHY_READ(addr, len)
+
+/* feedback from device units */
+#  define BX_INSTR_INP(addr, len)
+#  define BX_INSTR_INP2(addr, len, val)
+#  define BX_INSTR_OUTP(addr, len)
+#  define BX_INSTR_OUTP2(addr, len, val)
+
+#else   
+
+/* simulation init, shutdown, reset */
+#  define BX_INSTR_INIT(cpu_id)
+#  define BX_INSTR_SHUTDOWN(cpu_id)
+#  define BX_INSTR_RESET(cpu_id)
+#  define BX_INSTR_NEW_INSTRUCTION(cpu_id)
+
+/* called from command line debugger */
+#  define BX_INSTR_DEBUG_PROMPT()
+#  define BX_INSTR_START()
+#  define BX_INSTR_STOP()
+#  define BX_INSTR_PRINT()
+
+/* branch resoultion */
+#  define BX_INSTR_CNEAR_BRANCH_TAKEN(cpu_id, new_eip)
+#  define BX_INSTR_CNEAR_BRANCH_NOT_TAKEN(cpu_id)
+#  define BX_INSTR_UCNEAR_BRANCH(cpu_id, what, new_eip)
+#  define BX_INSTR_FAR_BRANCH(cpu_id, what, new_cs, new_eip)
+
+/* decoding completed */
+#  define BX_INSTR_OPCODE(cpu_id, opcode, len, is32) 
+#  define BX_INSTR_FETCH_DECODE_COMPLETED(cpu_id, i)
+     
+/* prefix decoded */
+#  define BX_INSTR_PREFIX_AS(cpu_id)
+#  define BX_INSTR_PREFIX_OS(cpu_id)
+#  define BX_INSTR_PREFIX_REP(cpu_id)
+#  define BX_INSTR_PREFIX_REPNE(cpu_id)
+#  define BX_INSTR_PREFIX_LOCK(cpu_id)
+#  define BX_INSTR_PREFIX_CS(cpu_id)
+#  define BX_INSTR_PREFIX_SS(cpu_id)
+#  define BX_INSTR_PREFIX_DS(cpu_id)
+#  define BX_INSTR_PREFIX_ES(cpu_id)
+#  define BX_INSTR_PREFIX_FS(cpu_id)
+#  define BX_INSTR_PREFIX_GS(cpu_id)
+#  define BX_INSTR_PREFIX_EXTEND8B(cpu_id)
+
+/* exceptional case and interrupt */
+#  define BX_INSTR_EXCEPTION(cpu_id, vector)
+#  define BX_INSTR_INTERRUPT(cpu_id, vector)
+#  define BX_INSTR_HWINTERRUPT(cpu_id, vector, cs, eip)
+
+/* TLB/CACHE control instruction executed */
+#  define BX_INSTR_CACHE_CNTRL(cpu_id, what)
+#  define BX_INSTR_TLB_CNTRL(cpu_id, what, newval)
+
+#  define BX_INSTR_REPEAT_ITERATION(cpu_id)
+
+/* memory access */
+#  define BX_INSTR_LIN_READ(cpu_id, lin, phy, len)
+#  define BX_INSTR_LIN_WRITE(cpu_id, lin, phy, len)
+
+#  define BX_INSTR_MEM_CODE(cpu_id, linear, size)      
+#  define BX_INSTR_MEM_DATA(cpu_id, linear, size, rw)
+
+/* called from memory object */
+#  define BX_INSTR_PHY_WRITE(addr, len)
+#  define BX_INSTR_PHY_READ(addr, len)
+
+/* feedback from device units */
+#  define BX_INSTR_INP(addr, len)
+#  define BX_INSTR_INP2(addr, len, val)
+#  define BX_INSTR_OUTP(addr, len)
+#  define BX_INSTR_OUTP2(addr, len, val)
+
+#endif  
