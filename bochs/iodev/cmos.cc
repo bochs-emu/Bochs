f2b4a6b3d376a3c386858b7d8e641c18f7a1ceaf
Bryce Denney <bryce@tlw.com>
2002-12-06 22:37:37 +0000

    - remove tabs

--- bochs/iodev/cmos.cc
+++ bochs/iodev/cmos.cc
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: cmos.cc,v 1.31 2002-12-06 22:36:48 bdenney Exp $
+// $Id: cmos.cc,v 1.32 2002-12-06 22:37:37 bdenney Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2002  MandrakeSoft S.A.
@@ -108,7 +108,7 @@ bx_cmos_c::~bx_cmos_c(void)
   void
 bx_cmos_c::init(void)
 {
-  BX_DEBUG(("Init $Id: cmos.cc,v 1.31 2002-12-06 22:36:48 bdenney Exp $"));
+  BX_DEBUG(("Init $Id: cmos.cc,v 1.32 2002-12-06 22:37:37 bdenney Exp $"));
   // CMOS RAM & RTC
 
   DEV_register_ioread_handler(this, read_handler, 0x0070, "CMOS RAM", 7);
@@ -119,12 +119,12 @@ bx_cmos_c::init(void)
   if (BX_CMOS_THIS s.periodic_timer_index == BX_NULL_TIMER_HANDLE) {
     BX_CMOS_THIS s.periodic_timer_index =
       DEV_register_timer(this, periodic_timer_handler,
-	1000000, 1,0, "cmos"); // continuous, not-active
+        1000000, 1,0, "cmos"); // continuous, not-active
   }
   if (BX_CMOS_THIS s.one_second_timer_index == BX_NULL_TIMER_HANDLE) {
     BX_CMOS_THIS s.one_second_timer_index =
       DEV_register_timer(this, one_second_timer_handler,
-	1000000, 1,0, "cmos"); // continuous, not-active
+        1000000, 1,0, "cmos"); // continuous, not-active
   }
 
 #if BX_USE_SPECIFIED_TIME0 == 0
