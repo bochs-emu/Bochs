39ab1d18acc642c3470074a0f48dec4d9d4c523c
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2003-05-23 08:49:55 +0000

    Fixed compilation error

--- bochs/cpu/3dnow.cc
+++ bochs/cpu/3dnow.cc
@@ -18,11 +18,11 @@
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 //
 
-#if BX_SUPPORT_3DNOW
-
 #define NEED_CPU_REG_SHORTCUTS 1
 #include "bochs.h"
 #define LOG_THIS BX_CPU_THIS_PTR
+
+#if BX_SUPPORT_3DNOW
 
 void BX_CPU_C::PFPNACC_PqQq(bxInstruction_c *i)
 {
