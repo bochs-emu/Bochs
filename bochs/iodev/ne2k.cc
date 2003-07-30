c496962184f14d219654d6d3bac1089a0d144892
Volker Ruppert <Volker.Ruppert@t-online.de>
2003-07-30 10:45:13 +0000

    - the NE2000 driver for MS-DOS writes a 16 bit value to page 0 reg 5. Now the   low-byte is written to this register and the high-byte is ignored.

--- bochs/iodev/ne2k.cc
+++ bochs/iodev/ne2k.cc
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: ne2k.cc,v 1.52 2003-06-07 19:16:54 vruppert Exp $
+// $Id: ne2k.cc,v 1.53 2003-07-30 10:45:13 vruppert Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2002  MandrakeSoft S.A.
@@ -512,10 +512,10 @@ bx_ne2k_c::page0_write(Bit32u offset, Bit32u value, unsigned io_len)
 {
   BX_DEBUG(("page 0 write to port %04x, len=%u", (unsigned) offset,
 	   (unsigned) io_len));
-    if (io_len != 1) {
-	BX_ERROR(("bad length! page 0 write to port %04x, len=%u",
-		  (unsigned) offset,
-            (unsigned) io_len));
+  // The NE2000 driver for MS-DOS writes a 16 bit value to reg 5
+  // ignoring the high-byte - is this okay ?
+  if (((io_len > 1) && (offset != 5)) || (io_len > 2)) {
+    BX_ERROR(("bad length! page 0 write to port %04x, len=%u", offset, io_len));
     return;
   }
 	
@@ -1272,7 +1272,7 @@ bx_ne2k_c::rx_frame(const void *buf, unsigned io_len)
 void
 bx_ne2k_c::init(void)
 {
-  BX_DEBUG(("Init $Id: ne2k.cc,v 1.52 2003-06-07 19:16:54 vruppert Exp $"));
+  BX_DEBUG(("Init $Id: ne2k.cc,v 1.53 2003-07-30 10:45:13 vruppert Exp $"));
 
   // Read in values from config file
   BX_NE2K_THIS s.base_address = bx_options.ne2k.Oioaddr->get ();
