76330672ebd15f54e5bc26008c656f3d0df15280
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2003-10-03 17:40:26 +0000

    FPU version change

--- bochs/fpu/version.h
+++ bochs/fpu/version.h
@@ -1,6 +1,6 @@
 /*---------------------------------------------------------------------------+
  |  version.h                                                                |
- |  $Id: version.h,v 1.2 2001-10-06 03:53:46 bdenney Exp $
+ |  $Id: version.h,v 1.3 2003-10-03 17:40:26 sshwarts Exp $
  |                                                                           |
  |                                                                           |
  | Copyright (C) 1992,1993,1994,1996,1997,1999                               |
@@ -10,4 +10,4 @@
  |                                                                           |
  +---------------------------------------------------------------------------*/
 
-#define FPU_VERSION "wm-FPU-emu version 2.05"
+#define FPU_VERSION "wm-FPU-emu version 2.1 for Bochs"
