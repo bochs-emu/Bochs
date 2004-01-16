3b17e2daa0ed0a444f77804032ba64f1de4cc8bb
Volker Ruppert <Volker.Ruppert@t-online.de>
2004-01-16 18:18:38 +0000

    - the console beep currently works on Linux only and cannot be a plugin yet - Bochs license and RCS ID added

--- bochs/iodev/console.cc
+++ bochs/iodev/console.cc
@@ -1,10 +1,25 @@
-/*
-Copyright 2003 by David N. Welton <davidw@dedasys.com>.
-
-This code may be distributed under the same terms as bochs.
-*/
+/////////////////////////////////////////////////////////////////////////
+// $Id: console.cc,v 1.2 2004-01-16 18:18:38 vruppert Exp $
+/////////////////////////////////////////////////////////////////////////
+//
+//  Copyright 2003 by David N. Welton <davidw@dedasys.com>.
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 
 #include "bochs.h"
+#ifdef __linux__
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
@@ -13,37 +28,44 @@ This code may be distributed under the same terms as bochs.
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <linux/kd.h>
+#endif
 
 bx_console_c::bx_console_c() {
-    this->put("CONSOLE");
-    access = 0;
-    consolefd = open("/dev/console", O_WRONLY);
-    if (consolefd != -1) {
-	access = 1;
-	this->info("Opened /dev/console");
-    } else {
-	this->info("Failed to open /dev/console: %s", strerror(errno));
-    }
+  this->put("CONSOLE");
+  access = 0;
+  consolefd = open("/dev/console", O_WRONLY);
+  if (consolefd != -1) {
+    access = 1;
+    this->info("Opened /dev/console");
+  } else {
+    this->info("Failed to open /dev/console: %s", strerror(errno));
+  }
 }
 
 bx_console_c::~bx_console_c() {
-    if (access) {
-	ioctl(consolefd, KIOCSOUND, 0);
-	close(consolefd);
-    }
+  if (access) {
+#ifdef __linux__
+    ioctl(consolefd, KIOCSOUND, 0);
+    close(consolefd);
+#endif
+  }
 }
 
 void bx_console_c::beep_on(float frequency)
 {
-    if (access) {
-	this->info("pc speaker on with frequency %f", frequency);
-        ioctl(consolefd, KIOCSOUND, (int)(clock_tick_rate/frequency));
-    }
+  if (access) {
+#ifdef __linux__
+    this->info("pc speaker on with frequency %f", frequency);
+    ioctl(consolefd, KIOCSOUND, (int)(clock_tick_rate/frequency));
+#endif
+  }
 }
 
 void bx_console_c::beep_off()
 {
-    if (access) {
-	ioctl(consolefd, KIOCSOUND, 0);
-    }
+  if (access) {
+#ifdef __linux__
+    ioctl(consolefd, KIOCSOUND, 0);
+#endif
+  }
 }
