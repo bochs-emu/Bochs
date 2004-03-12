967297b8a4215a870f4d2ff6e2b2c5299b24f9ef
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2004-03-12 20:08:50 +0000

    new version of softfloat-fpu branch currectly stable

---
+++ bochs/cpu/fpu_trans.cc
@@ -0,0 +1,161 @@
+/////////////////////////////////////////////////////////////////////////
+//  Copyright (C) 2004  MandrakeSoft S.A.
+//
+//    MandrakeSoft S.A.
+//    43, rue d'Aboukir
+//    75002 Paris - France
+//    http://www.linux-mandrake.com/
+//    http://www.mandrakesoft.com/
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+/////////////////////////////////////////////////////////////////////////
+
+
+#define NEED_CPU_REG_SHORTCUTS 1
+#include "bochs.h"
+#define LOG_THIS BX_CPU_THIS_PTR
+
+
+void BX_CPU_C::FSIN(bxInstruction_c *i)
+{
+#if BX_SUPPORT_FPU
+  BX_CPU_THIS_PTR prepareFPU(i);
+
+  fpu_execute(i);
+//#else
+  BX_INFO(("FSIN: required FPU, configure --enable-fpu"));
+#endif
+}
+
+void BX_CPU_C::FCOS(bxInstruction_c *i)
+{
+#if BX_SUPPORT_FPU
+  BX_CPU_THIS_PTR prepareFPU(i);
+
+  fpu_execute(i);
+//#else
+  BX_INFO(("FCOS: required FPU, configure --enable-fpu"));
+#endif
+}
+
+void BX_CPU_C::FSINCOS(bxInstruction_c *i)
+{
+#if BX_SUPPORT_FPU
+  BX_CPU_THIS_PTR prepareFPU(i);
+
+  fpu_execute(i);
+//#else
+  BX_INFO(("FSINCOS: required FPU, configure --enable-fpu"));
+#endif
+}
+
+void BX_CPU_C::FPTAN(bxInstruction_c *i)
+{
+#if BX_SUPPORT_FPU
+  BX_CPU_THIS_PTR prepareFPU(i);
+
+  fpu_execute(i);
+//#else
+  BX_INFO(("FPTAN: required FPU, configure --enable-fpu"));
+#endif
+}
+
+void BX_CPU_C::FPATAN(bxInstruction_c *i)
+{
+#if BX_SUPPORT_FPU
+  BX_CPU_THIS_PTR prepareFPU(i);
+
+  fpu_execute(i);
+//#else
+  BX_INFO(("FPATAN: required FPU, configure --enable-fpu"));
+#endif
+}
+
+void BX_CPU_C::FYL2XP1(bxInstruction_c *i)
+{
+#if BX_SUPPORT_FPU
+  BX_CPU_THIS_PTR prepareFPU(i);
+
+  fpu_execute(i);
+//#else
+  BX_INFO(("FYL2XP1: required FPU, configure --enable-fpu"));
+#endif
+}
+
+void BX_CPU_C::F2XM1(bxInstruction_c *i)
+{
+#if BX_SUPPORT_FPU
+  BX_CPU_THIS_PTR prepareFPU(i);
+
+  fpu_execute(i);
+//#else
+  BX_INFO(("F2XM1: required FPU, configure --enable-fpu"));
+#endif
+}
+
+void BX_CPU_C::FYL2X(bxInstruction_c *i)
+{
+#if BX_SUPPORT_FPU
+  BX_CPU_THIS_PTR prepareFPU(i);
+
+  fpu_execute(i);
+//#else
+  BX_INFO(("FYL2X: required FPU, configure --enable-fpu"));
+#endif
+}
+
+void BX_CPU_C::FSCALE(bxInstruction_c *i)
+{
+#if BX_SUPPORT_FPU
+  BX_CPU_THIS_PTR prepareFPU(i);
+
+  fpu_execute(i);
+//#else
+  BX_INFO(("FSCALE: required FPU, configure --enable-fpu"));
+#endif
+}
+
+void BX_CPU_C::FXTRACT(bxInstruction_c *i)
+{
+#if BX_SUPPORT_FPU
+  BX_CPU_THIS_PTR prepareFPU(i);
+
+  fpu_execute(i);
+//#else
+  BX_INFO(("FXTRACT: required FPU, configure --enable-fpu"));
+#endif
+}
+
+void BX_CPU_C::FPREM1(bxInstruction_c *i)
+{
+#if BX_SUPPORT_FPU
+  BX_CPU_THIS_PTR prepareFPU(i);
+
+  fpu_execute(i);
+//#else
+  BX_INFO(("FPREM1: required FPU, configure --enable-fpu"));
+#endif
+}
+
+void BX_CPU_C::FPREM(bxInstruction_c *i)
+{
+#if BX_SUPPORT_FPU
+  BX_CPU_THIS_PTR prepareFPU(i);
+
+  fpu_execute(i);
+//#else
+  BX_INFO(("FPREM: required FPU, configure --enable-fpu"));
+#endif
+}
---
+++ bochs/cpu/softfloat-fpu.h
@@ -0,0 +1,104 @@
+/*============================================================================
+This source file is an addon to the SoftFloat IEC/IEEE Floating-point
+Arithmetic Package, Release 2b, written for Bochs (x86 achitecture simulator)
+floating point emulation.
+
+THIS SOFTWARE IS DISTRIBUTED AS IS, FOR FREE.  Although reasonable effort has
+been made to avoid it, THIS SOFTWARE MAY CONTAIN FAULTS THAT WILL AT TIMES
+RESULT IN INCORRECT BEHAVIOR.  USE OF THIS SOFTWARE IS RESTRICTED TO PERSONS
+AND ORGANIZATIONS WHO CAN AND WILL TAKE FULL RESPONSIBILITY FOR ALL LOSSES,
+COSTS, OR OTHER PROBLEMS THEY INCUR DUE TO THE SOFTWARE, AND WHO FURTHERMORE
+EFFECTIVELY INDEMNIFY JOHN HAUSER AND THE INTERNATIONAL COMPUTER SCIENCE
+INSTITUTE (possibly via similar legal warning) AGAINST ALL LOSSES, COSTS, OR
+OTHER PROBLEMS INCURRED BY THEIR CUSTOMERS AND CLIENTS DUE TO THE SOFTWARE.
+
+Derivative works are acceptable, even for commercial purposes, so long as
+(1) the source code for the derivative work includes prominent notice that
+the work is derivative, and (2) the source code includes prominent notice with
+these four paragraphs for those parts of this code that are retained.
+=============================================================================*/
+
+/*============================================================================
+ * Adapted for Bochs (x86 achitecture simulator) by
+ *            Stanislav Shwartsman (gate@fidonet.org.il)
+ * ==========================================================================*/ 
+
+#include "softfloat.h"
+
+/*----------------------------------------------------------------------------
+| Functions and definitions to determine:  (1) whether tininess for underflow
+| is detected before or after rounding by default, (2) what (if anything)
+| happens when exceptions are raised, (3) how signaling NaNs are distinguished
+| from quiet NaNs, (4) the default generated quiet NaNs, and (5) how NaNs
+| are propagated from function inputs to output.  These details are target-
+| specific.
+*----------------------------------------------------------------------------*/
+#include "softfloat-specialize.h"
+
+/*-----------------------------------------------------------------------------
+| Calculates the absolute value of the extended double-precision floating-point
+| value `a'.  The operation is performed according to the IEC/IEEE Standard
+| for Binary Floating-Point Arithmetic.
+*----------------------------------------------------------------------------*/
+
+BX_CPP_INLINE floatx80& floatx80_abs(floatx80 &reg)
+{
+    reg.exp &= 0x7FFF;
+    return reg;   
+}
+
+/*-----------------------------------------------------------------------------
+| Changes the sign of the extended double-precision floating-point value 'a'.
+| The operation is performed according to the IEC/IEEE Standard for Binary 
+| Floating-Point Arithmetic.
+*----------------------------------------------------------------------------*/
+
+BX_CPP_INLINE floatx80& floatx80_chs(floatx80 &reg)
+{
+    reg.exp ^= 0x8000;
+    return reg;   
+}
+
+/*----------------------------------------------------------------------------
+| Returns the result of converting the extended double-precision floating-
+| point value `a' to the 16-bit two's complement integer format.  The
+| conversion is performed according to the IEC/IEEE Standard for Binary
+| Floating-Point Arithmetic - which means in particular that the conversion
+| is rounded according to the current rounding mode. If `a' is a NaN or the 
+| conversion overflows, the integer indefinite value is returned.
+*----------------------------------------------------------------------------*/
+
+BX_CPP_INLINE Bit16s floatx80_to_int16(floatx80 a, float_status_t &status)
+{
+   Bit32s v32 = floatx80_to_int32(a, status);
+
+   if ((v32 > (Bit32s) BX_MAX_BIT16S) || (v32 < (Bit32s) BX_MIN_BIT16S))
+   {
+        float_raise(status, float_flag_invalid);
+        return int16_indefinite;
+   }
+
+   return (Bit16s) v32;
+}
+
+/*----------------------------------------------------------------------------
+| Returns the result of converting the extended double-precision floating-
+| point value `a' to the 16-bit two's complement integer format.  The
+| conversion is performed according to the IEC/IEEE Standard for Binary
+| Floating-Point Arithmetic, except that the conversion is always rounded
+| toward zero.  If `a' is a NaN or the conversion overflows, the integer 
+| indefinite value is returned.
+*----------------------------------------------------------------------------*/
+
+BX_CPP_INLINE Bit16s floatx80_to_int16_round_to_zero(floatx80 a, float_status_t &status)
+{
+   Bit32s v32 = floatx80_to_int32_round_to_zero(a, status);
+
+   if ((v32 > (Bit32s) BX_MAX_BIT16S) || (v32 < (Bit32s) BX_MIN_BIT16S))
+   {
+        float_raise(status, float_flag_invalid);
+        return int16_indefinite;
+   }
+
+   return (Bit16s) v32;
+}
