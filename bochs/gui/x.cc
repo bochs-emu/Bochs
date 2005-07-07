f412af1e6b4faeee991824a0e5f275d378f4b6c6
Volker Ruppert <Volker.Ruppert@t-online.de>
2005-07-07 07:17:02 +0000

    - backported rev. 1.90 and 1.91

--- bochs/gui/x.cc
+++ bochs/gui/x.cc
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: x.cc,v 1.89 2005-04-29 19:06:24 sshwarts Exp $
+// $Id: x.cc,v 1.89.2.1 2005-07-07 07:17:02 vruppert Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2002  MandrakeSoft S.A.
@@ -1790,11 +1790,11 @@ static void disable_cursor ()
 	     mask_width = 16,
 	     mask_height = 16;
 
-      static uint32 shape_bits[(16*16)/32] = {
+      static Bit32u shape_bits[(16*16)/32] = {
 	    0x00000000, 0x00000000, 0x00000000, 0x00000000,
 	    0x00000000, 0x00000000, 0x00000000, 0x00000000,
       };
-      static uint32 mask_bits[(16*16)/32] = {
+      static Bit32u mask_bits[(16*16)/32] = {
 	    0x00000000, 0x00000000, 0x00000000, 0x00000000,
 	    0x00000000, 0x00000000, 0x00000000, 0x00000000,
       };
@@ -1899,8 +1899,11 @@ Bool XPeekEventTimeout( Display *display, XEvent *event_return, struct timeval *
       switch(res)
       {
         case -1: /* select() error - should not happen */ 
-            perror("XPeekEventTimeout: select() failure"); 
-            return(False);
+          if (errno == EINTR) 
+             break; // caused e.g. by alarm(3)
+          perror("XPeekEventTimeout: select() failure"); 
+          return(False);
+
         case  0: /* timeout */
           return(False);
       }
@@ -1910,7 +1913,6 @@ Bool XPeekEventTimeout( Display *display, XEvent *event_return, struct timeval *
     return(True);
 }
 
-#if BX_USE_IDLE_HACK
 void bx_x_gui_c::sim_is_idle () {
   XEvent dummy;
   struct timeval   timeout;   
@@ -1918,7 +1920,6 @@ void bx_x_gui_c::sim_is_idle () {
   timeout.tv_usec = 1000; /* 1/1000 s */  
   XPeekEventTimeout(bx_x_display, &dummy, &timeout);
 }
-#endif
 #endif /* BX_USE_IDLE_HACK */  
 
 
