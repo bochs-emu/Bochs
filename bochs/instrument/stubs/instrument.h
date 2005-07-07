991acc6829408826305f636fcebb0539696fbaf4
Volker Ruppert <Volker.Ruppert@t-online.de>
2005-07-07 07:15:21 +0000

    - backported rev. 1.18

--- bochs/instrument/stubs/instrument.h
+++ bochs/instrument/stubs/instrument.h
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: instrument.h,v 1.17 2005-04-29 21:28:59 sshwarts Exp $
+// $Id: instrument.h,v 1.17.2.1 2005-07-07 07:15:21 vruppert Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2001  MandrakeSoft S.A.
@@ -80,9 +80,9 @@ void bx_instr_tlb_cntrl(unsigned cpu, unsigned what, Bit32u newval);
 void bx_instr_cache_cntrl(unsigned cpu, unsigned what);
 void bx_instr_prefetch_hint(unsigned cpu, unsigned what, unsigned seg, bx_address offset);
 
-void bx_instr_before_execution(unsigned cpu);
-void bx_instr_after_execution(unsigned cpu);
-void bx_instr_repeat_iteration(unsigned cpu);
+void bx_instr_before_execution(unsigned cpu, const bxInstruction_c *i);
+void bx_instr_after_execution(unsigned cpu, const bxInstruction_c *i);
+void bx_instr_repeat_iteration(unsigned cpu, const bxInstruction_c *i);
 
 void bx_instr_inp(Bit16u addr, unsigned len);
 void bx_instr_outp(Bit16u addr, unsigned len);
