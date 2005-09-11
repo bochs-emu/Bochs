95b12d7429dc5da28f9e2577205399b17de8cd8d
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2005-09-11 20:00:29 +0000

    #SF patch fixed transition from vm8086 to PM

--- bochs/cpu/cpu.h
+++ bochs/cpu/cpu.h
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: cpu.h,v 1.240 2005-09-03 11:39:26 sshwarts Exp $
+// $Id: cpu.h,v 1.241 2005-09-11 20:00:29 sshwarts Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2001  MandrakeSoft S.A.
@@ -415,8 +415,11 @@ typedef struct {
   BX_CPP_INLINE void BX_CPU_C::setEFlags(Bit32u val) {                       \
     BX_CPU_THIS_PTR eflags.val32 = val;                                      \
     BX_CPU_THIS_PTR eflags.VM_cached = val & (1<<17);                        \
-    if (BX_CPU_THIS_PTR cr0.pe && BX_CPU_THIS_PTR eflags.VM_cached) {        \
-      BX_CPU_THIS_PTR cpu_mode = BX_MODE_IA32_V8086;                         \
+    if (BX_CPU_THIS_PTR cr0.pe) {                                            \
+      if (BX_CPU_THIS_PTR eflags.VM_cached)                                  \
+        BX_CPU_THIS_PTR cpu_mode = BX_MODE_IA32_V8086;                       \
+      else                                                                   \
+        BX_CPU_THIS_PTR cpu_mode = BX_MODE_IA32_PROTECTED;                   \
     }                                                                        \
   }
 
@@ -481,8 +484,11 @@ typedef struct {
     BX_CPU_THIS_PTR eflags.val32 =                                           \
       (BX_CPU_THIS_PTR eflags.val32&~(1<<bitnum)) | (val ? (1<<bitnum) : 0); \
     BX_CPU_THIS_PTR eflags.VM_cached = val;                                  \
-    if (BX_CPU_THIS_PTR cr0.pe && BX_CPU_THIS_PTR eflags.VM_cached) {        \
-      BX_CPU_THIS_PTR cpu_mode = BX_MODE_IA32_V8086;                         \
+    if (BX_CPU_THIS_PTR cr0.pe) {                                            \
+      if (BX_CPU_THIS_PTR eflags.VM_cached)                                  \
+        BX_CPU_THIS_PTR cpu_mode = BX_MODE_IA32_V8086;                       \
+      else                                                                   \
+        BX_CPU_THIS_PTR cpu_mode = BX_MODE_IA32_PROTECTED;                   \
     }                                                                        \
   }
 
