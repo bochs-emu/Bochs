bd22a4e39a53adfa42b59c50929ce322ac07b64d
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2006-05-28 17:22:35 +0000

    Revert format change for now (I won't work for me). The problem that original '%x' also won't work ;( Need to find solution to print Bit64u !

--- bochs/gui/siminterface.cc
+++ bochs/gui/siminterface.cc
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: siminterface.cc,v 1.151 2006-05-28 16:39:25 vruppert Exp $
+// $Id: siminterface.cc,v 1.152 2006-05-28 17:22:35 sshwarts Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 // See siminterface.h for description of the siminterface concept.
@@ -1388,7 +1388,7 @@ bx_shadow_num_c::bx_shadow_num_c(bx_param_c *parent,
   val.p64bit = ptr_to_real_val;
   if (base == BASE_HEX) {
     this->base = base;
-    this->text_format = "0x" FMT_LL "x";
+    this->text_format = "0x%x";
   }
 }
 
@@ -1407,7 +1407,7 @@ bx_shadow_num_c::bx_shadow_num_c(bx_param_c *parent,
   val.p64bit = (Bit64s*) ptr_to_real_val;
   if (base == BASE_HEX) {
     this->base = base;
-    this->text_format = "0x " FMT_LL "x";
+    this->text_format = "0x%x";
   }
 }
 
