f091c7c0ad025828bf86bf7c066f0f32895c3b7e
Volker Ruppert <Volker.Ruppert@t-online.de>
2006-10-02 21:22:59 +0000

    - SMM must be enabled until all data is read from RAM

--- bochs/cpu/smm.cc
+++ bochs/cpu/smm.cc
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: smm.cc,v 1.20 2006-08-31 18:18:17 sshwarts Exp $
+// $Id: smm.cc,v 1.21 2006-10-02 21:22:59 vruppert Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //   Copyright (c) 2006 Stanislav Shwartsman
@@ -78,7 +78,6 @@ void BX_CPU_C::RSM(bxInstruction_c *i)
   BX_INFO(("RSM: Resuming from System Management Mode !"));
 
   BX_CPU_THIS_PTR nmi_disable = 0;
-  BX_CPU_THIS_PTR in_smm = 0;
 
   Bit32u saved_state[SMM_SAVE_STATE_MAP_SIZE], n;
   // reset reserved bits
@@ -90,6 +89,7 @@ void BX_CPU_C::RSM(bxInstruction_c *i)
     base -= 4;
     BX_CPU_THIS_PTR mem->readPhysicalPage(BX_CPU_THIS, base, 4, &saved_state[n]);
   }
+  BX_CPU_THIS_PTR in_smm = 0;
 
   // restore the CPU state from SMRAM
   if (! smram_restore_state(saved_state)) {
