f19595136bb2b5d695e95533b0bb5655691e6f78
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2006-11-07 08:24:22 +0000

    *** empty log message ***

--- bochs/bx_debug/dbg_main.cc
+++ bochs/bx_debug/dbg_main.cc
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: dbg_main.cc,v 1.85 2006-10-24 17:53:47 vruppert Exp $
+// $Id: dbg_main.cc,v 1.86 2006-11-07 08:24:22 sshwarts Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2001  MandrakeSoft S.A.
@@ -2944,9 +2944,9 @@ void bx_dbg_info_ivt_command(unsigned from, unsigned to)
       seg = (buff[3] << 8) | buff[2];
       off = (buff[1] << 8) | buff[0];
 #endif
-      BX_MEM(0)->dbg_fetch_mem(BX_CPU(dbg_cpu), idtr.base + ((seg << 4) + off), sizeof(buff), buff);
-      dbg_printf("INT# %02x > %04X:%04X (" FMT_ADDRX ") %s%s\n", i, seg, off, 
-         idtr.base + ((seg << 4) + off), bx_dbg_ivt_desc(i), 
+      BX_MEM(0)->dbg_fetch_mem(BX_CPU(dbg_cpu), ((seg << 4) + off), sizeof(buff), buff);
+      dbg_printf("INT# %02x > %04X:%04X (" FMT_ADDRX ") %s%s\n", i, seg, off,
+         ((seg << 4) + off), bx_dbg_ivt_desc(i),
          (buff[0] == 0xcf) ? " ; dummy iret" : "");
     }
     if (all) dbg_printf("You can list individual entries with 'info ivt [NUM]' or groups with 'info ivt [NUM] [NUM]'\n");
