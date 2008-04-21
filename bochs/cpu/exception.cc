b33d1a19a4dd1d840f2871df5a4e9120ff902479
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2008-04-21 21:29:43 +0000

    Correctly push error code after task switch in exception()

--- bochs/cpu/exception.cc
+++ bochs/cpu/exception.cc
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: exception.cc,v 1.111 2008-04-19 20:00:28 sshwarts Exp $
+// $Id: exception.cc,v 1.112 2008-04-21 21:29:43 sshwarts Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2001  MandrakeSoft S.A.
@@ -168,7 +168,7 @@ void BX_CPU_C::long_mode_int(Bit8u vector, bx_bool is_INT, bx_bool is_error_code
 
     // check selector and descriptor for new stack in current TSS
     if (ist != 0) {
-      BX_DEBUG(("interrupt(long mode): trap to IST, vector = %d\n",ist));
+      BX_DEBUG(("interrupt(long mode): trap to IST, vector = %d", ist));
       get_RSP_from_TSS(ist+3, &RSP_for_cpl_x);
     }
     else {
@@ -394,7 +394,7 @@ void BX_CPU_C::protected_mode_int(Bit8u vector, bx_bool is_INT, bx_bool is_error
     // push error code onto stack
 
     if (is_error_code) {
-      if (BX_CPU_THIS_PTR sregs[BX_SEG_REG_CS].cache.u.segment.d_b)
+      if (tss_descriptor.type >= 9) // TSS386
         push_32(error_code);
       else
         push_16(error_code);
