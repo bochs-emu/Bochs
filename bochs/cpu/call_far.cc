6865edb5ee22ec36fe4904d95c4238502bba695b
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2008-05-12 06:12:12 +0000

    Fixed pushing to stack in call far through 64-bit call gate

--- bochs/cpu/call_far.cc
+++ bochs/cpu/call_far.cc
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////
-// $Id: call_far.cc,v 1.35 2008-05-10 20:35:03 sshwarts Exp $
+// $Id: call_far.cc,v 1.36 2008-05-12 06:12:12 sshwarts Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //   Copyright (c) 2005 Stanislav Shwartsman
@@ -578,9 +578,10 @@ BX_CPU_C::call_gate64(bx_selector_t *gate_selector)
   {
     BX_DEBUG(("CALL GATE TO SAME PRIVILEGE"));
 
-    // push return address onto stack
-    push_64(old_CS);
-    push_64(old_RIP);
+    // push to 64-bit stack, switch to long64 guaranteed
+    write_new_stack_qword_64(RSP -  8, CPL, old_CS);
+    write_new_stack_qword_64(RSP - 16, CPL, old_RIP);
+    RSP -= 16;
 
     // load CS:RIP (guaranteed to be in 64 bit mode)
     branch_far64(&cs_selector, &cs_descriptor, new_RIP, CPL);
