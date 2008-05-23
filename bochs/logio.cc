94edd3cf3c8e045a48aa03a6c62ecf216ca04358
Volker Ruppert <Volker.Ruppert@t-online.de>
2008-05-23 08:51:31 +0000

    - store prefix and message in 'exit_msg' before unloading device plugins.   A device panic or APM / ACPI power off caused an access violation when writing   to console with plugins enabled, since bx_atexit() unloads them.

--- bochs/logio.cc
+++ bochs/logio.cc
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: logio.cc,v 1.67 2008-02-15 22:05:37 sshwarts Exp $
+// $Id: logio.cc,v 1.68 2008-05-23 08:51:31 vruppert Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2001  MandrakeSoft S.A.
@@ -579,6 +579,14 @@ static void carbonFatalDialog(const char *error, const char *exposition)
 void logfunctions::fatal(const char *prefix, const char *fmt, va_list ap, int exit_status)
 {
 #if !BX_DEBUGGER
+#if !BX_WITH_WX
+  // store prefix and message in 'exit_msg' before unloading device plugins
+  char tmpbuf[1024];
+  char exit_msg[1024];
+
+  vsprintf(tmpbuf, fmt, ap);
+  sprintf(exit_msg, "%s %s", prefix, tmpbuf);
+#endif
   bx_atexit();
 #endif
 #if BX_WITH_CARBON
@@ -596,8 +604,7 @@ void logfunctions::fatal(const char *prefix, const char *fmt, va_list ap, int ex
   static const char *divider = "========================================================================";
   fprintf(stderr, "%s\n", divider);
   fprintf(stderr, "Bochs is exiting with the following message:\n");
-  fprintf(stderr, "%s ", prefix);
-  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "%s", exit_msg);
   fprintf(stderr, "\n%s\n", divider);
 #endif
 #if !BX_DEBUGGER
