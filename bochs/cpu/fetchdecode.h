de5814a22d22e877321ec92fb4935e76ce713a2a
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2009-01-31 15:01:29 +0000

    Fixed compilation err

--- bochs/cpu/fetchdecode.h
+++ bochs/cpu/fetchdecode.h
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: fetchdecode.h,v 1.81 2009-01-31 10:43:23 sshwarts Exp $
+// $Id: fetchdecode.h,v 1.82 2009-01-31 15:01:29 sshwarts Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //   Copyright (c) 2005 Stanislav Shwartsman
@@ -1768,6 +1768,12 @@ static const BxOpcodeInfo_t BxOpcodeGroupSSE_G1407[3] = {
   /* F3 */ { 0, BX_IA_ERROR }
 };
 
+static const BxOpcodeInfo_t BxOpcodeGroupSSE_G9VMX6[3] = {
+  /* 66 */ { 0, BX_IA_VMCLEAR_Mq },
+  /* F2 */ { 0, BX_IA_ERROR },
+  /* F3 */ { 0, BX_IA_VMXON_Mq }
+};
+
 #if (BX_SUPPORT_SSE >= 4) || (BX_SUPPORT_SSE >= 3 && BX_SUPPORT_SSE_EXTENSION > 0)
 
 static const BxOpcodeInfo_t BxOpcodeGroupSSE_0f3800[3] = {
@@ -2254,12 +2260,6 @@ static const BxOpcodeInfo_t BxOpcodeGroupSSE_0f3adf[3] = {
   /* 66 */ { 0, BX_IA_AESKEYGENASSIST_VdqWdqIb },
   /* F2 */ { 0, BX_IA_ERROR },
   /* F3 */ { 0, BX_IA_ERROR }
-};
-
-static const BxOpcodeInfo_t BxOpcodeGroupSSE_G9VMX6[3] = {
-  /* 66 */ { 0, BX_IA_VMCLEAR_Mq },
-  /* F2 */ { 0, BX_IA_ERROR },
-  /* F3 */ { 0, BX_IA_VMXON_Mq }
 };
 
 
