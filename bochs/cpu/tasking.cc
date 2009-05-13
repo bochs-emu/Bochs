0b59fdfe31fffce3cfc7934360f159287c4b986b
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2009-05-13 14:31:09 +0000

    small fix

--- bochs/cpu/tasking.cc
+++ bochs/cpu/tasking.cc
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: tasking.cc,v 1.73 2009-05-07 12:02:34 sshwarts Exp $
+// $Id: tasking.cc,v 1.74 2009-05-13 14:31:09 sshwarts Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2001  MandrakeSoft S.A.
@@ -218,7 +218,7 @@ void BX_CPU_C::task_switch(bxInstruction_c *i, bx_selector_t *tss_selector,
     if (source == BX_TASK_FROM_CALL || source == BX_TASK_FROM_INT)
     {
       dtranslate_linear(nbase32,     0, BX_WRITE);
-      dtranslate_linear(nbase32 + 2, 0, BX_WRITE);
+      dtranslate_linear(nbase32 + 1, 0, BX_WRITE);
     }
   }
 
