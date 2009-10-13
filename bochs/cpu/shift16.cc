2a8b7e04bae9e99fb83e3cb799d775cf840238b4
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2009-10-13 15:28:16 +0000

    bugfix

--- bochs/cpu/shift16.cc
+++ bochs/cpu/shift16.cc
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: shift16.cc,v 1.50 2009-06-20 20:39:51 sshwarts Exp $
+// $Id: shift16.cc,v 1.51 2009-10-13 15:28:16 sshwarts Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2001  MandrakeSoft S.A.
@@ -476,7 +476,7 @@ void BX_CPP_AttrRegparmN(1) BX_CPU_C::SHR_Ew(bxInstruction_c *i)
   cf = (op1_16 >> (count - 1)) & 0x1;
   // note, that of == result15 if count == 1 and
   //            of == 0        if count >= 2
-  of = ((result_16 << 1) ^ result_16) >> 15;
+  of = (((result_16 << 1) ^ result_16) >> 15) & 0x1;
 
   SET_FLAGS_OSZAPC_LOGIC_16(result_16);
   SET_FLAGS_OxxxxC(of, cf);
