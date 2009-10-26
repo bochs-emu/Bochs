54cdee30c15bb73ec40d0213789cbb6fb9625ce7
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2009-10-26 22:05:00 +0000

    fix dbg method as well

--- bochs/cpu/paging.cc
+++ bochs/cpu/paging.cc
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: paging.cc,v 1.187 2009-10-26 21:19:23 sshwarts Exp $
+// $Id: paging.cc,v 1.188 2009-10-26 22:05:00 sshwarts Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2001  MandrakeSoft S.A.
@@ -1451,7 +1451,7 @@ bx_bool BX_CPU_C::dbg_xlate_linear2phy(bx_address laddr, bx_phy_address *phy)
       if (level == 1 && (pte & 0x80)) { // PSE page
 	offset_mask = 0x3fffff;
 #if BX_PHY_ADDRESS_WIDTH > 32
-        pt_address += ((bx_phy_address)(pte & 0x003f7000)) << 19;
+        pt_address += ((bx_phy_address)(pte & 0x003fe000)) << 19;
 #endif
 	break;
       }
