aa07cd0324e4a6c89bffc248f0cf97acc9ed4f8a
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2009-12-30 19:21:46 +0000

    Tables generated in rombios32 were overwriten by the IPL code in rombios. This should partially fix bug #1933859.
    - Sebastian

--- bochs/bios/rombios.c
+++ bochs/bios/rombios.c
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: rombios.c,v 1.239 2009-12-27 17:03:14 sshwarts Exp $
+// $Id: rombios.c,v 1.240 2009-12-30 19:21:46 sshwarts Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2002  MandrakeSoft S.A.
@@ -169,14 +169,14 @@
 #define EBDA_SIZE          1              // In KiB
 #define BASE_MEM_IN_K   (640 - EBDA_SIZE)
 
-/* 256 bytes at 0x9ff00 -- 0x9ffff is used for the IPL boot table. */
+/* IPL_SIZE bytes at 0x9ff00 are used for the IPL boot table. */
 #define IPL_SEG              0x9ff0
 #define IPL_TABLE_OFFSET     0x0000
 #define IPL_TABLE_ENTRIES    8
 #define IPL_COUNT_OFFSET     0x0080  /* u16: number of valid table entries */
 #define IPL_SEQUENCE_OFFSET  0x0082  /* u16: next boot device */
 #define IPL_BOOTFIRST_OFFSET 0x0084  /* u16: user selected device */
-#define IPL_SIZE             0xff
+#define IPL_SIZE             0x86
 #define IPL_TYPE_FLOPPY      0x01
 #define IPL_TYPE_HARDDISK    0x02
 #define IPL_TYPE_CDROM       0x03
@@ -927,7 +927,7 @@ Bit16u cdrom_boot();
 
 #endif // BX_ELTORITO_BOOT
 
-static char bios_cvs_version_string[] = "$Revision: 1.239 $ $Date: 2009-12-27 17:03:14 $";
+static char bios_cvs_version_string[] = "$Revision: 1.240 $ $Date: 2009-12-30 19:21:46 $";
 
 #define BIOS_COPYRIGHT_STRING "(c) 2002 MandrakeSoft S.A. Written by Kevin Lawton & the Bochs team."
 
--- bochs/bios/rombios32.c
+++ bochs/bios/rombios32.c
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: rombios32.c,v 1.57 2009-12-28 20:08:30 sshwarts Exp $
+// $Id: rombios32.c,v 1.58 2009-12-30 19:21:46 sshwarts Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //  32 bit Bochs BIOS init code
@@ -602,7 +602,7 @@ void ram_probe(void)
     ram_end = ram_size;
   BX_INFO("ram_end=%ldMB\n", ram_end >> 20);
 #ifdef BX_USE_EBDA_TABLES
-  ebda_cur_addr = ((*(uint16_t *)(0x40e)) << 4) + 0x380;
+  ebda_cur_addr = ((*(uint16_t *)(0x40e)) << 4) + 0x386;
   BX_INFO("ebda_cur_addr: 0x%08lx\n", ebda_cur_addr);
 #endif
 }
