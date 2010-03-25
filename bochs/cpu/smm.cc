f675bb852c3e8e8c9fcd45a7013aa1bac7cda732
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2010-03-25 21:39:34 +0000

    setCR3 change

--- bochs/cpu/smm.cc
+++ bochs/cpu/smm.cc
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: smm.cc,v 1.66 2010-03-25 21:33:07 sshwarts Exp $
+// $Id: smm.cc,v 1.67 2010-03-25 21:39:34 sshwarts Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //   Copyright (c) 2006-2009 Stanislav Shwartsman
@@ -756,7 +756,10 @@ bx_bool BX_CPU_C::smram_restore_state(const Bit32u *saved_state)
     BX_PANIC(("SMM restore: failed to restore CR0 !"));
     return 0;
   }
-  SetCR3(temp_cr3);
+  if (!SetCR3(temp_cr3)) {
+    BX_PANIC(("SMM restore: failed to restore CR3 !"));
+    return 0;
+  }
   setEFlags(temp_eflags);
 
 #if BX_CPU_LEVEL >= 4
