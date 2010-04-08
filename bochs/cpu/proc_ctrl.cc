50462dde9ae18995299d7531cf11666c559f5da8
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2010-04-08 16:38:41 +0000

    RDTSCP could be run outside long64

--- bochs/cpu/proc_ctrl.cc
+++ bochs/cpu/proc_ctrl.cc
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: proc_ctrl.cc,v 1.331 2010-04-04 18:46:03 sshwarts Exp $
+// $Id: proc_ctrl.cc,v 1.332 2010-04-08 16:38:41 sshwarts Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2001-2010  The Bochs Project
@@ -634,9 +634,6 @@ void BX_CPP_AttrRegparmN(1) BX_CPU_C::RDTSC(bxInstruction_c *i)
 #if BX_SUPPORT_X86_64
 void BX_CPP_AttrRegparmN(1) BX_CPU_C::RDTSCP(bxInstruction_c *i)
 {
-  if(! long64_mode())
-    exception(BX_UD_EXCEPTION, 0);
-
 #if BX_SUPPORT_VMX
   // RDTSCP will always #UD in legacy VMX mode
   if (BX_CPU_THIS_PTR in_vmx_guest) {
