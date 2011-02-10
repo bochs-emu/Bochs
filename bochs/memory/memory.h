2d57f0c3e9fd62cd135ff0187ea3004120f9a26f
Volker Ruppert <Volker.Ruppert@t-online.de>
2011-02-10 23:00:16 +0000

    - moved array 'rom_present' to the memory class and initialize it at startup

--- bochs/memory/memory.h
+++ bochs/memory/memory.h
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////
-// $Id: memory.h,v 1.72 2010-08-15 19:57:49 sshwarts Exp $
+// $Id: memory.h,v 1.73 2011-02-10 23:00:16 vruppert Exp $
 /////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (C) 2001-2009  The Bochs Project
@@ -73,6 +73,7 @@ private:
   Bit8u   *rom;      // 512k BIOS rom space + 128k expansion rom space
   Bit8u   *bogus;    // 4k for unexisting memory
   unsigned used_blocks;
+  bx_bool rom_present[65];
 
 public:
   BX_MEM_C();
