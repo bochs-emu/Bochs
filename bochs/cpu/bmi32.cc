ddecc0234adf5e88937d2068d12b835f82a690a2
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2011-10-20 14:06:12 +0000

    fixed (c) info

--- bochs/cpu/bmi32.cc
+++ bochs/cpu/bmi32.cc
@@ -2,7 +2,8 @@
 // $Id$
 /////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (C) 2011  The Bochs Project
+//   Copyright (c) 2011 Stanislav Shwartsman
+//          Written by Stanislav Shwartsman [sshwarts at sourceforge net]
 //
 //  This library is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU Lesser General Public
