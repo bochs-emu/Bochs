bff3ba1535acbcaec487071d38e8a3bfcb7cbfd2
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2012-09-06 19:49:14 +0000

    small optimization in lazy flags code

--- bochs/cpu/lazy_flags.h
+++ bochs/cpu/lazy_flags.h
@@ -53,7 +53,7 @@ typedef struct {
   (((op1) & (op2)) | (((op1) | (op2)) & (~(result))))
 
 #define SUB_COUT_VEC(op1, op2, result) \
-  (((~(op1)) & (op2)) | ((~((op1) ^ (op2))) & (result)))
+  (((~(op1)) & (op2)) | (((~(op1)) ^ (op2)) & (result)))
 
 #define GET_ADD_OVERFLOW(op1, op2, result, mask) \
   ((((op1) ^ (result)) & ((op2) ^ (result))) & (mask))
