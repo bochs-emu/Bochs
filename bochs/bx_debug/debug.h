4bed791ccbd0dd6fe452214a7c443c01044c35e4
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2013-01-19 20:45:03 +0000

    Added year 2013 to Copyright in all files already modified in new year

--- bochs/bx_debug/debug.h
+++ bochs/bx_debug/debug.h
@@ -2,7 +2,7 @@
 // $Id$
 /////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (C) 2001-2011  The Bochs Project
+//  Copyright (C) 2001-2013  The Bochs Project
 //
 //  This library is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU Lesser General Public
