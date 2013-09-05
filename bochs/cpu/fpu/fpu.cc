0fd4e3450c6705c024e3b8eaffeb4c52429e8907
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2013-09-05 18:40:14 +0000

    update (c) for few files

--- bochs/cpu/fpu/fpu.cc
+++ bochs/cpu/fpu/fpu.cc
@@ -2,7 +2,7 @@
 // $Id$
 /////////////////////////////////////////////////////////////////////////
 //
-//   Copyright (c) 2003-2009 Stanislav Shwartsman
+//   Copyright (c) 2003-2013 Stanislav Shwartsman
 //          Written by Stanislav Shwartsman [sshwarts at sourceforge net]
 //
 //  This library is free software; you can redistribute it and/or
