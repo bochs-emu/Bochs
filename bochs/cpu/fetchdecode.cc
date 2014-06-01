0b2364501d4bc18d64918dcde060099f58266d04
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2014-06-01 10:46:17 +0000

    fixed compilation err when 3dnow is ON

--- bochs/cpu/fetchdecode.cc
+++ bochs/cpu/fetchdecode.cc
@@ -1841,7 +1841,7 @@ modrm_done:
 
 #if BX_SUPPORT_3DNOW
   if(b1 == 0x10f)
-    ia_opcode = Bx3DNowOpcode[i->modRMForm.Ib];
+    ia_opcode = Bx3DNowOpcode[i->modRMForm.Ib[0]];
 #endif
 
   // assign sources
