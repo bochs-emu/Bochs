54072a74755a421d9978ee41fd96b6234d6491e2
Volker Ruppert <Volker.Ruppert@t-online.de>
2014-08-03 15:15:36 +0000

    Enable ask dialog bx default if the SDL2 gui is present

--- bochs/logio.cc
+++ bochs/logio.cc
@@ -277,7 +277,7 @@ int logfunctions::default_onoff[N_LOGLEV] =
   ACT_IGNORE,  // ignore debug
   ACT_REPORT,  // report info
   ACT_REPORT,  // report error
-#if BX_WITH_WX || BX_WITH_WIN32 || BX_WITH_X11
+#if BX_WITH_SDL2 || BX_WITH_WX || BX_WITH_WIN32 || BX_WITH_X11
   ACT_ASK      // on panic, ask user what to do
 #else
   ACT_FATAL    // on panic, quit
