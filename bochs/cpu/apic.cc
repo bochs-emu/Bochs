29efae3be38d0a28563f144f781d20b1bc9837f3
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2014-08-31 20:05:25 +0000

    adjust (c) in several files

--- bochs/cpu/apic.cc
+++ bochs/cpu/apic.cc
@@ -2,7 +2,7 @@
 // $Id$
 /////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2002-2012 Zwane Mwaikambo, Stanislav Shwartsman
+//  Copyright (c) 2002-2014 Zwane Mwaikambo, Stanislav Shwartsman
 //
 //  This library is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU Lesser General Public
