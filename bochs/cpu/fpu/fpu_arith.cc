e357da915049d14e2c51bb21ea350c60762d72a6
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2017-05-05 21:09:27 +0000

    update (c) for fpu files

--- bochs/cpu/fpu/fpu_arith.cc
+++ bochs/cpu/fpu/fpu_arith.cc
@@ -2,7 +2,7 @@
 // $Id$
 /////////////////////////////////////////////////////////////////////////
 //
-//   Copyright (c) 2003-2015 Stanislav Shwartsman
+//   Copyright (c) 2003-2017 Stanislav Shwartsman
 //          Written by Stanislav Shwartsman [sshwarts at sourceforge net]
 //
 //  This library is free software; you can redistribute it and/or
