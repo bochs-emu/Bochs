a7e58973ce983742b18f2f3080f82465a9577af5
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2017-11-27 20:26:54 +0000

    fixed typo

--- bochs/cpu/decoder/fetchdecode.h
+++ bochs/cpu/decoder/fetchdecode.h
@@ -177,7 +177,7 @@ BX_CPP_INLINE Bit64u FetchQWORD(const Bit8u *iptr)
 #define BX_TRACE_END                 (0x01)
 
 struct bxIAOpcodeTable {
-#ifdef BX_STANDALONE_DECODER
+#ifndef BX_STANDALONE_DECODER
   BxExecutePtr_tR execute1;
   BxExecutePtr_tR execute2;
 #endif
