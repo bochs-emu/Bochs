ed8fa8ac610465750f5ba962989e437b2ca30607
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2017-12-24 15:38:21 +0000

    fix compilation with no AVX enabled

--- bochs/cpu/decoder/disasm.cc
+++ bochs/cpu/decoder/disasm.cc
@@ -254,9 +254,11 @@ char *resolve_memsize(char *disbufptr, const bxInstruction_c *i, unsigned src_in
       break;
     }
   }
+#if BX_SUPPORT_AVX
   else if (src_index == BX_SRC_VSIB) {
     disbufptr = dis_sprintf(disbufptr, "%sword ptr ", intel_vector_reg_name[i->getVL() - 1]);
   }
+#endif
 
   return disbufptr;
 }
