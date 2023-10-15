2d5c198e8b6b3b87122044f65af5f19d470e5450
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2023-10-15 08:07:40 +0300

    implemented Linear Address Separation extension (LASS)
    # Conflicts:
    #	bochs/CHANGES

--- bochs/cpu/cpu.cc
+++ bochs/cpu/cpu.cc
@@ -557,6 +557,14 @@ void BX_CPU_C::prefetch(void)
       exception(BX_GP_EXCEPTION, 0);
     }
 
+    if (BX_CPU_THIS_PTR cr4.get_LASS()) {
+      // RIP[63] == 0 user, RIP[63] == 1 supervisor
+      if ((RIP >> 63) == USER_PL) {
+        BX_ERROR(("prefetch: #GP(0): LASS violation during fetch CPL=%d RIP=0x" FMT_PHY_ADDRX, CPL, RIP));
+        exception(BX_GP_EXCEPTION, 0);
+      }
+    }
+
     // linear address is equal to RIP in 64-bit long mode
     pageOffset = PAGE_OFFSET(EIP);
     laddr = RIP;
