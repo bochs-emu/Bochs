2e23f15234acb453e1ce53dc8617525048001946
MVoloshin <maxvoloshin71@gmail.com>
2024-12-29 17:36:58 +0300

    Avoid relative patches in Android build scripts

--- bochs/build/android/build-core.sh
+++ bochs/build/android/build-core.sh
@@ -4,14 +4,14 @@ LOCAL_PATH=`dirname $0`
 LOCAL_PATH=`cd $LOCAL_PATH && pwd`
 
 if [ \! -d bochs/bochs ] ; then
-   ln -s ../../../../bochs bochs
+   ln -s $ANDROID_BOCHS_HOME bochs
 fi
 
-if [ \! -d ../../../androidsdl/project/jni/application/bochs ] ; then
-   ln -s ../../../../bochs/build/android/bochs ../../../androidsdl/project/jni/application
+if [ \! -d $ANDROID_SDL_HOME/project/jni/application/bochs ] ; then
+   ln -s $ANDROID_BOCHS_HOME/build/android/bochs $ANDROID_SDL_HOME/project/jni/application
 fi
 
-cd ../../../androidsdl
+cd $ANDROID_SDL_HOME
 ./build.sh -s release bochs
-mv project/app/build/outputs/apk/release/app-release.apk ../bochs/build/android/bochs-core-debug.apk
-cd ../bochs/build/android
+mv project/app/build/outputs/apk/release/app-release.apk $ANDROID_BOCHS_HOME/build/android/bochs-core-debug.apk
+cd $ANDROID_BOCHS_HOME/build/android
