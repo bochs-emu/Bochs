bochs-emu/Bochs

commit 1e3561f82cbe1cbc979d789e3fdf54f3d2aa0ab2

Added "globalini" extension to the new command line option "-dbg_gui". Removed "gui_debug" display library option. Fixed some warnings.

bochs/gui/sdl.cc
@@ -507,7 +507,7 @@ void bx_sdl_gui_c::specific_init(int argc, char **argv, unsigned headerbar_y)
     SIM->set_debug_gui(1);
 #ifdef WIN32
     if (gui_ci) {
-      sdl_enh_dbg_global_ini = gui_opts.enh_dbg_global_ini;
+      sdl_enh_dbg_global_ini = bx_dbg.dbg_gui_globalini;
       // on Windows the debugger gui must run in a separate thread
       DWORD threadID;
       CreateThread(NULL, 0, DebugGuiThread, NULL, 0, &threadID);
@@ -515,7 +515,7 @@ void bx_sdl_gui_c::specific_init(int argc, char **argv, unsigned headerbar_y)
       BX_PANIC(("Config interface 'win32config' is required for gui debugger"));
     }
 #else
-    init_debug_dialog(gui_opts.enh_dbg_global_ini);
+    init_debug_dialog(bx_dbg._dbg_gui_globalini);
 #endif
   }
 #endif

bochs/.bochsrc
@@ -72,15 +72,14 @@
 # "cmdmode"     - call a headerbar button handler after pressing F7 (sdl, sdl2,
 #                 win32, x)
 # "fullscreen"  - startup in fullscreen mode (sdl, sdl2)
-# "gui_debug"   - use GTK debugger gui (sdl, sdl2, x) / Win32 debugger gui (sdl,
-#                 sdl2, win32). When using the "globalini" extension the
-#                 debugger uses ini file from BXSHARE path (see below).
 # "hideIPS"     - disable IPS output in status bar (rfb, sdl, sdl2, term, vncsrv,
 #                 win32, wx, x)
 # "nokeyrepeat" - turn off host keyboard repeat (sdl, sdl2, win32, x)
 # "no_gui_console" - use system console instead of builtin gui console
 #                    (rfb, sdl, sdl2, vncsrv, x)
 # "timeout"     - time (in seconds) to wait for client (rfb, vncsrv)
+# "gui_debug"   - This option is DEPRECATED, use command line option '-dbg_gui'
+#                 instead. It also supports the 'globalini' extension
 #
 # See the examples below for other currently supported options.
 # Setting up options without specifying display library is also supported.
@@ -101,7 +100,7 @@
 #                 on desktop window size
 #display_library: win32, options="traphotkeys, autoscale"
 #display_library: wx
-#display_library: x, options="gui_debug:globalini"
+#display_library: x
 
 #=======================================================================
 # CPU:

bochs/bochs.h
@@ -311,6 +311,7 @@ typedef struct {
 #if BX_DEBUGGER
 #if BX_DEBUGGER_GUI
   bool debugger_gui;
+  bool dbg_gui_globalini;
 #endif
   Bit8u magic_break;
 #endif

bochs/doc/docbook/user/user.dbk
@@ -3315,11 +3315,12 @@ behaviour. These options are supported by more than one display library:
 <screen>
   "cmdmode"     - call a headerbar button handler after pressing F7 (sdl, sdl2, win32, x)
   "fullscreen"  - startup in fullscreen mode (sdl, sdl2)
-  "gui_debug"   - use GTK debugger GUI (sdl, x) / Win32 debugger GUI (sdl, sdl2, win32)
   "hideIPS"     - disable IPS output in status bar (rfb, sdl, sdl2, term, vncsrv, win32, wx, x)
   "nokeyrepeat" - turn off host keyboard repeat (sdl, sdl2, win32, x)
   "no_gui_console" - use system console instead of builtin GUI console (rfb, sdl, sdl2, vncsrv, x)
   "timeout"     - time (in seconds) to wait for client (rfb, vncsrv)
+  "gui_debug"   - This option is DEPRECATED, use command line option '-dbg_gui'
+                  instead. It also supports the 'globalini' extension
 </screen>
 See the examples below for other currently supported options.
 <screen>
@@ -5437,6 +5438,10 @@ For other arguments, see section <link linkend="bochsrc">bochsrc</link>.
 <row>
   <entry>-dbg_gui</entry>
   <entry>start Bochs internal debugger with gui on startup</entry>
+</row>
+<row>
+  <entry>-dbg_gui:globalini</entry>
+  <entry>start Bochs internal debugger gui using ini file from BXSHARE path</entry>
 </row>
 <row>
   <entry>-dbglog <replaceable>filename</replaceable></entry>

bochs/doc/man/bochs.1
@@ -1,5 +1,5 @@
 .\"Document Author:  Timothy R. Butler   -   tbutler@uninetsolutions.com"
-.TH bochs 1 "04 Mar 2025" "bochs @version@" "The Bochs Project"
+.TH bochs 1 "07 Mar 2025" "bochs @version@" "The Bochs Project"
 .\"SKIP_SECTION"
 .SH NAME
 bochs \- Portable x86 Emulator.
@@ -70,6 +70,9 @@ Start Bochs internal debugger on startup
 .TP
 .BI \-dbg_gui
 Start Bochs internal debugger with gui on startup
+.TP
+.BI \-dbg_gui:globalini
+Start Bochs internal debugger gui using ini file from BXSHARE path
 .TP
 .BI \-rc\ filename
 Execute debugger commands stored in file

bochs/doc/man/bochsrc.5
@@ -1,5 +1,5 @@
 .\"Document Author:  Timothy R. Butler   -   tbutler@uninetsolutions.com"
-.TH bochsrc 5 "15 Feb 2025" "bochsrc" "The Bochs Project"
+.TH bochsrc 5 "07 Mar 2025" "bochsrc" "The Bochs Project"
 .\"SKIP_SECTION"
 .SH NAME
 bochsrc \- Configuration file for Bochs.
@@ -111,11 +111,11 @@ behaviour. These options are supported by more than one display library:
 
   "cmdmode"     - call a headerbar button handler after pressing F7 (x, sdl, sdl2)
   "fullscreen"  - startup in fullscreen mode (sdl, sdl2)
-  "gui_debug"   - use GTK debugger gui (sdl, sdl2, x)
   "hideIPS"     - disable IPS output in status bar (rfb, sdl, sdl2, term, vncsrv, wx, x)
   "nokeyrepeat" - turn off host keyboard repeat (sdl, sdl2, x)
   "no_gui_console" - use system console instead of builtin gui console (rfb, sdl, sdl2, vncsrv, x)
   "timeout"     - time (in seconds) to wait for client (rfb, vncsrv)
+  "gui_debug"   - This option is DEPRECATED, use command line option '-dbg_gui' instead. It also supports the 'globalini' extension
 
 .B NOTE:
 Setting up options without specifying display library is also supported.

bochs/gui/enh_dbg.cc
@@ -8,7 +8,7 @@
 //
 //  Modified by Bruce Ewing
 //
-//  Copyright (C) 2008-2024  The Bochs Project
+//  Copyright (C) 2008-2025  The Bochs Project
 
 #include "config.h"
 
@@ -26,6 +26,8 @@
 extern char* disasm(const Bit8u *opcode, bool is_32, bool is_64, char *disbufptr, bxInstruction_c *i, bx_address cs_base, bx_address rip);
 
 #include "enh_dbg.h"
+
+#define LOG_THIS genlog->
 
 // Match stuff
 #define MATCH_TRUE 1
@@ -238,7 +240,7 @@ bx_phy_address WWP_Snapshot[16];
 bx_phy_address RWP_Snapshot[16];
 
 bool global_ini;
-char ini_path[BX_PATHNAME_LEN];
+char ini_path[BX_PATHNAME_LEN + 16];
 char *debug_cmd;
 bool debug_cmd_ready;
 bool vgaw_refresh;
@@ -1652,7 +1654,7 @@ void FillPAGE()
         if(valid) {
             if((lin - start_lin) != (phy - start_phy)) {
                 if(start_lin != 1) {
-                    sprintf (pa_lin,"0x%08X - 0x%08X",start_lin, lin - 1);
+                    sprintf (pa_lin,"0x" FMT_ADDRX64 " - 0x" FMT_ADDRX64, start_lin, lin - 1);
                     sprintf (pa_phy,"0x" FMT_LLCAPX " - 0x" FMT_LLCAPX,
                         start_phy, start_phy + (lin-1-start_lin));
                     AddPagingLine (LineCount,pa_lin,pa_phy);
@@ -1663,7 +1665,7 @@ void FillPAGE()
             }
         } else {
             if(start_lin != 1) {
-                sprintf (pa_lin,"0x%08X - 0x%08X",start_lin, lin - 1);
+                sprintf (pa_lin,"0x" FMT_ADDRX64 " - 0x" FMT_ADDRX64, start_lin, lin - 1);
                 sprintf (pa_phy,"0x" FMT_LLCAPX " - 0x" FMT_LLCAPX,
                     start_phy, start_phy + (lin-1-start_lin));
                 AddPagingLine (LineCount,pa_lin,pa_phy);
@@ -1684,7 +1686,7 @@ void FillPAGE()
         lin++;
     }
     if(start_lin != 1) {
-        sprintf (pa_lin,"0x%08X - 0x%08X", start_lin, -1);
+        sprintf (pa_lin,"0x" FMT_ADDRX64 " - 0x%08X", start_lin, -1);
         sprintf (pa_phy,"0x" FMT_LLCAPX " - 0x" FMT_LLCAPX,start_phy, start_phy + (lin-1-start_lin));
         AddPagingLine (LineCount,pa_lin,pa_phy);
     }
@@ -3513,6 +3515,9 @@ void ReadSettings()
   if (fd == NULL) {
     fd = fopen("bx_enh_dbg.ini", "r");
     if (fd == NULL) return;
+    BX_INFO(("Reading gui debugger settings from local ini file"));
+  } else {
+    BX_INFO(("Reading gui debugger settings from '%s'", ini_path));
   }
   do {
     ret = fgets(line, sizeof(line)-1, fd);
@@ -3600,10 +3605,14 @@ void WriteSettings()
 
   if (global_ini) {
     fd = fopen(ini_path, "w");
-  } else {
-    fd = fopen("bx_enh_dbg.ini", "w");
   }
-  if (fd == NULL) return;
+  if (fd == NULL) {
+    fd = fopen("bx_enh_dbg.ini", "w");
+    if (fd == NULL) return;
+    BX_INFO(("Saving gui debugger settings to local ini file"));
+  } else {
+    BX_INFO(("Saving gui debugger settings to '%s'", ini_path));
+  }
   fprintf(fd, "# bx_enh_dbg_ini\n");
   for (i = 0; i < 8; i++) {
     fprintf(fd, "SeeReg[%d] = %s\n", i, SeeReg[i] ? "TRUE" : "FALSE");

bochs/gui/gui.cc
@@ -1656,22 +1656,11 @@ bool bx_gui_c::parse_common_gui_options(const char *arg, Bit8u flags)
     return true;
   } else if (!strncmp(arg, "gui_debug", 9)) {
 #if BX_DEBUGGER && BX_DEBUGGER_GUI
-    bx_dbg.debugger_gui = 1;
-    gui_opts.enh_dbg_global_ini = 0;
-    if ((strlen(arg) > 9) && (arg[9] == ':')) {
-      if (!strcmp(&arg[10], "globalini")) {
-        gui_opts.enh_dbg_global_ini = 1;
-        BX_INFO(("Debugger gui using global config from BXSHARE path"));
-      } else {
-        BX_ERROR(("Ignoring unknown setting '%s' for gui debugger", &arg[10]));
-      }
-    } else if (strlen(arg) > 9) {
-      return false;
-    }
+    SIM->message_box("ERROR", "The 'gui_debug' option is deprecated - use command line option '-dbg_gui' instead");
 #else
     SIM->message_box("ERROR", "Bochs debugger not available - ignoring 'gui_debug' option");
 #endif
-    return true;
+    return false;
 #if BX_SHOW_IPS
   } else if (!strcmp(arg, "hideIPS") && (flags & BX_GUI_OPT_HIDE_IPS)) {
     BX_INFO(("hide IPS display in status bar"));

commit 33925155fe6c8b0f7d8e7bb7969c1dba672cc50d

Fix linux compilation bug introduced in 1e3561f.

bochs/gui/sdl.cc
@@ -515,7 +515,7 @@ void bx_sdl_gui_c::specific_init(int argc, char **argv, unsigned headerbar_y)
       BX_PANIC(("Config interface 'win32config' is required for gui debugger"));
     }
 #else
-    init_debug_dialog(bx_dbg._dbg_gui_globalini);
+    init_debug_dialog(bx_dbg.dbg_gui_globalini);
 #endif
   }
 #endif