a274a9532a7728ba6cee0d9edb2c8c9eec30e48f
Volker Ruppert <Volker.Ruppert@t-online.de>
2025-05-15 22:22:41 +0200

    Fixed compilation after PR #550.

--- bochs/bios/rombios.c
+++ bochs/bios/rombios.c
@@ -5932,7 +5932,6 @@ int13_harddisk(DS, ES, DI, SI, BP, ELDX, BX, DX, CX, AX, IP, CS, FLAGS)
 
     case 0x09: /* initialize drive parameters */
     case 0x0c: /* seek to specified cylinder */
-    case 0x0d: /* alternate disk reset */
     case 0x11: /* recalibrate */
     case 0x14: /* controller internal diagnostic */
       BX_INFO("int13_harddisk: function %02xh unimplemented, returns success\n", GET_AH());
