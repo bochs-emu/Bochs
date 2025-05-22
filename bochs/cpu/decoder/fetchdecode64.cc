59a2b093d52a15f02248533c61a4f3919cc8fe4d
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2025-05-22 18:28:55 +0300

    EVEX.U and SAE for EVEX.256 is officially deprecated

--- bochs/cpu/decoder/fetchdecode64.cc
+++ bochs/cpu/decoder/fetchdecode64.cc
@@ -916,7 +916,7 @@ int decoder_evex64(const Bit8u *iptr, unsigned &remain, bxInstruction_c *i, unsi
   //    7 6 5 4 3 2 1 0
   //    ---------------
   // P0 R X B R'0 m m m
-  // P1 w v v v v u p p ; evex.u was always '1 before avx10.2
+  // P1 w v v v v u p p
   // P2 z L'L b V'a a a
 
   // EVEX.mmmm - opcode group
@@ -929,11 +929,14 @@ int decoder_evex64(const Bit8u *iptr, unsigned &remain, bxInstruction_c *i, unsi
   // EVEX.W    - opsize promotion / opcode extension
   // EVEX.z    - zero masking / merging
   // EVEX.b    - broadcast / round control / SAE
-  // EVEX.u    - round control / SAE for 256-bit (AVX10.2)
   // EVEX.LL   - vector length control
 
   // check for reserved EVEX bits
   if ((evex & 0x08) != 0)
+    return(ia_opcode);
+
+  // EVEX.U must be '1
+  if ((evex & 0x400) == 0)
     return(ia_opcode);
 
   unsigned evex_opc_map = evex & 0x7;
@@ -968,9 +971,6 @@ int decoder_evex64(const Bit8u *iptr, unsigned &remain, bxInstruction_c *i, unsi
   i->setVL(1 << evex_vl_rc);
   i->setVexW(vex_w);
 
-  unsigned evex_u = (evex >> 10) & 0x1;
-  i->setEvexU(evex_u);
-
   unsigned evex_z = (evex >> 23) & 0x1;
   i->setZeroMasking(evex_z);
 
@@ -994,9 +994,6 @@ int decoder_evex64(const Bit8u *iptr, unsigned &remain, bxInstruction_c *i, unsi
   if (i->modC0()) {
     // EVEX.b in reg form implies 512-bit vector length
     if (i->getEvexb()) i->setVL(BX_VL512);
-
-    // EVEX.u=0 in reg form implies 256-bit vector length
-    if (!i->getEvexU()) i->setVL(BX_VL256);
   }
 
   Bit32u vl = i->getVL()-1; // 0: VL128, 1: VL256, 3: VL512
