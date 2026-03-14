ebe3b9155a5ebf5abdfa1134876d635a51b9287c
Stanislav Shwartsman <sshwarts@users.sourceforge.net>
2026-03-14 10:22:09 +0200

    change BX_INFO message to BX_DEBUG to avoid unnecessary log polution

--- bochs/cpu/msr.cc
+++ bochs/cpu/msr.cc
@@ -359,7 +359,7 @@ bool BX_CPP_AttrRegparmN(2) BX_CPU_C::rdmsr(Bit32u index, Bit64u *msr)
 #if BX_SUPPORT_APIC
     case BX_MSR_APICBASE:
       val64 = BX_CPU_THIS_PTR msr.apicbase;
-      BX_INFO(("RDMSR: Read %08x:%08x from MSR_APICBASE", GET32H(val64), GET32L(val64)));
+      BX_DEBUG(("RDMSR: Read %08x:%08x from MSR_APICBASE", GET32H(val64), GET32L(val64)));
       break;
 #endif
 
