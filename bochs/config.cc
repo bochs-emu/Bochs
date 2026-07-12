d84848c398a8f23a20c92f85dbf792b4e4461f6f
Volker Ruppert <Volker.Ruppert@t-online.de>
2026-07-12 06:10:01 +0200

    Attempt to fix issue #783. config.cc: Don't reset network parameters if compiled without network.

--- bochs/config.cc
+++ bochs/config.cc
@@ -1775,8 +1775,10 @@ void bx_reset_options()
   // serial/parallel/usb
   SIM->get_param("ports")->reset();
 
+#if BX_NETWORKING
   // network devices
   SIM->get_param("network")->reset();
+#endif
 
   // sound devices
   SIM->get_param("sound")->reset();
