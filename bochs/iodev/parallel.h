da8feda3ed21851d14f52d1f994524e558c8473b
Volker Ruppert <Volker.Ruppert@t-online.de>
2026-07-30 22:17:14 +0200

    Compilation fixes using BX_PAR_SMF / BX_SER_SMF for the static case.

--- bochs/iodev/parallel.h
+++ bochs/iodev/parallel.h
@@ -72,7 +72,7 @@ public:
 private:
   bx_par_t s[BX_PARPORT_MAXDEV];
 
-  void   virtual_printer(Bit8u port);
+  BX_PAR_SMF void virtual_printer(Bit8u port);
 
   static Bit32u read_handler(void *this_ptr, Bit32u address, unsigned io_len);
   static void   write_handler(void *this_ptr, Bit32u address, Bit32u value, unsigned io_len);
